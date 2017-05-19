c42145d4a4bacf82dea834119ce067994bf538ac
shibdib <supernfadmin@gmail.com>
2017-05-19 04:54:43 -0400

    camelCase is hard

--- inUse/module.respawn.js
+++ inUse/module.respawn.js
@@ -2,33 +2,33 @@
  * Created by rober on 5/16/2017.
  */
 
+////////////////////////////////////////////Vars//////////////////////////////////////////////////
+
+//Stationary Creeps
+let stationaryHarvester = _.filter(Game.creeps, (creep) => creep.memory.role === 'stationaryHarvester');
+let stationaryBuilders = _.filter(Game.creeps, (creep) => creep.memory.role === 'stationaryBuilder');
+
+//Worker Creeps
+let worker = _.filter(Game.creeps, (creep) => creep.memory.role === 'worker');
+let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
+let wallRepairers = _.filter(Game.creeps, (creep) => creep.memory.role === 'wallRepairer');
+
+//Hauling Creeps
+let haulers = _.filter(Game.creeps, (creep) => creep.memory.role === 'hauler');
+let expediters = _.filter(Game.creeps, (creep) => creep.memory.role === 'expediter');
+let dumpTrucks = _.filter(Game.creeps, (creep) => creep.memory.role === 'dumpTruck');
+
+//Combat Creeps
+let rangedDefenders = _.filter(Game.creeps, (creep) => creep.memory.role === 'rangedDefender');
+let defenders = _.filter(Game.creeps, (creep) => creep.memory.role === 'defender');
+let scout = _.filter(Game.creeps, (creep) => creep.memory.role === 'scout');
+
+//MISC
+let sourceCount = Game.spawns['spawn1'].room.find(FIND_SOURCES);
+
 const respawnCreeps = {
     /** @param  {Spawn} spawn  **/
     run: function () {
-
-        ////////////////////////////////////////////Vars//////////////////////////////////////////////////
-
-        //Stationary Creeps
-        var stationaryHarvester = _.filter(Game.creeps, (creep) => creep.memory.role === 'stationaryHarvester');
-        var stationaryBuilders = _.filter(Game.creeps, (creep) => creep.memory.role === 'stationaryBuilder');
-
-        //Worker Creeps
-        var worker = _.filter(Game.creeps, (creep) => creep.memory.role === 'worker');
-        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
-        var wallRepairers = _.filter(Game.creeps, (creep) => creep.memory.role === 'wallRepairer');
-
-        //Hauling Creeps
-        var haulers = _.filter(Game.creeps, (creep) => creep.memory.role === 'hauler');
-        var Expediters = _.filter(Game.creeps, (creep) => creep.memory.role === 'expediter');
-        var dumpTrucks = _.filter(Game.creeps, (creep) => creep.memory.role === 'dumpTruck');
-
-        //Combat Creeps
-        var rangedDefenders = _.filter(Game.creeps, (creep) => creep.memory.role === 'rangedDefender');
-        var Defenders = _.filter(Game.creeps, (creep) => creep.memory.role === 'defender');
-        var scout = _.filter(Game.creeps, (creep) => creep.memory.role === 'scout');
-
-        //MISC
-        var sourceCount = Game.spawns['spawn1'].room.find(FIND_SOURCES);
 
         ///////////////////////////////////////////////COUNT TO CONSOLE///////////////////////////////////////
 
@@ -46,63 +46,63 @@ const respawnCreeps = {
         //COMBAT RESPAWNS
         if (Game.flags.combatBuild) {
             if (rangedDefenders.length < 3) {
-                var newName = Game.spawns['spawn1'].createCreep([RANGED_ATTACK, RANGED_ATTACK, MOVE, MOVE, MOVE], undefined, {role: 'rangedDefender'});
-            } else if (Defenders.length < 4 && Game.spawns['spawn1'].energyCapacity >= 350) {
-                var newName = Game.spawns['spawn1'].createCreep([ATTACK, ATTACK, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE], undefined, {role: 'defender'});
+                let newName = Game.spawns['spawn1'].createCreep([RANGED_ATTACK, RANGED_ATTACK, MOVE, MOVE, MOVE], undefined, {role: 'rangedDefender'});
+            } else if (defenders.length < 4 && Game.spawns['spawn1'].energyCapacity >= 350) {
+                let newName = Game.spawns['spawn1'].createCreep([ATTACK, ATTACK, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE], undefined, {role: 'defender'});
             }
         }
 
         //HAULER RESPAWNS
         if (Game.flags.haulerBuild) {
-            var containers = Game.spawns['spawn1'].room.find(FIND_STRUCTURES, {
+            let containers = Game.spawns['spawn1'].room.find(FIND_STRUCTURES, {
                 filter: { structureType: STRUCTURE_CONTAINER }
             });
-            if (Expediters.length < containers.length) {
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, MOVE, MOVE, MOVE, MOVE], undefined, {role: 'expediter'});
+            if (expediters.length < containers.length) {
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, MOVE, MOVE, MOVE, MOVE], undefined, {role: 'expediter'});
             } else if (haulers.length < containers.length) {
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, MOVE, MOVE, MOVE], undefined, {role: 'hauler'});
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, MOVE, MOVE, MOVE], undefined, {role: 'hauler'});
             } else if (dumpTrucks.length < stationaryBuilders.length + upgraders.length) {
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, MOVE, MOVE, MOVE], undefined, {role: 'dumpTruck'});
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, MOVE, MOVE, MOVE], undefined, {role: 'dumpTruck'});
             }
         }
 
         //HARVESTER RESPAWNS
         if (Game.flags.harvesterBuild) {
             if (stationaryHarvester.length < Math.ceil(sourceCount.length * 3)) {
-                var newName = Game.spawns['spawn1'].createCreep([WORK, WORK, MOVE], undefined, {role: 'stationaryHarvester'});
+                let newName = Game.spawns['spawn1'].createCreep([WORK, WORK, MOVE], undefined, {role: 'stationaryHarvester'});
             }
         }
 
         //BUILDER RESPAWNS
         if (Game.flags.builderBuild) {
-            var constructionSites = sources = Game.spawns['spawn1'].room.find(FIND_CONSTRUCTION_SITES, {filter: (s) => s.structureType !== STRUCTURE_WALL || STRUCTURE_ROAD || STRUCTURE_RAMPART});
+            let constructionSites = sources = Game.spawns['spawn1'].room.find(FIND_CONSTRUCTION_SITES, {filter: (s) => s.structureType !== STRUCTURE_WALL || STRUCTURE_ROAD || STRUCTURE_RAMPART});
             if (constructionSites.length === 0 && stationaryBuilders.length < 2){
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, WORK, WORK, WORK, MOVE], undefined, {role: 'stationaryBuilder'});
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, WORK, WORK, WORK, MOVE], undefined, {role: 'stationaryBuilder'});
             }else
             if (stationaryBuilders.length < Math.ceil(constructionSites.length / 3)) {
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, WORK, WORK, WORK, MOVE], undefined, {role: 'stationaryBuilder'});
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, WORK, WORK, WORK, MOVE], undefined, {role: 'stationaryBuilder'});
             }
         }
 
         //WORKER RESPAWNS
         if (Game.flags.workerBuild) {
             if (worker.length < 3) {
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, WORK, WORK, MOVE], undefined, {role: 'worker'});
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, WORK, WORK, MOVE], undefined, {role: 'worker'});
             } else if (upgraders.length < 1) {
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, CARRY, WORK, WORK, WORK, WORK, WORK, WORK, MOVE], undefined, {role: 'upgrader'});
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, CARRY, CARRY, WORK, WORK, WORK, WORK, WORK, WORK, MOVE], undefined, {role: 'upgrader'});
             } else if (wallRepairers.length < 1) {
-                var newName = Game.spawns['spawn1'].createCreep([CARRY, WORK, WORK, MOVE], undefined, {role: 'wallRepairer'});
+                let newName = Game.spawns['spawn1'].createCreep([CARRY, WORK, WORK, MOVE], undefined, {role: 'wallRepairer'});
             }
         }
 
         //SCOUT RESPAWNS
         if (Game.flags.scoutBuild) {
-            for (var i = 0; i < 5; i++) {
-                var scout = 'scout' + i;
+            for (let i = 0; i < 5; i++) {
+                let scout = 'scout' + i;
                 if (Game.flags[scout]) {
-                    var creep = _.filter(Game.creeps, (creep) => creep.memory.destination === scout);
+                    let creep = _.filter(Game.creeps, (creep) => creep.memory.destination === scout);
                     if (creep.length === 0) {
-                        var newName = Game.spawns['spawn1'].createCreep([MOVE, MOVE, MOVE, MOVE], undefined, {
+                        let newName = Game.spawns['spawn1'].createCreep([MOVE, MOVE, MOVE, MOVE], undefined, {
                             role: 'scout',
                             destination: scout
                         });
@@ -113,7 +113,7 @@ const respawnCreeps = {
 
 
         if (Game.spawns['spawn1'].spawning) {
-            var spawningCreep = Game.creeps[Game.spawns['spawn1'].spawning.name];
+            let spawningCreep = Game.creeps[Game.spawns['spawn1'].spawning.name];
             Game.spawns['spawn1'].room.visual.text(
                 '🛠️' + spawningCreep.memory.role,
                 Game.spawns['spawn1'].pos.x + 1,
