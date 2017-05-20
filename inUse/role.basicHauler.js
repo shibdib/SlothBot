1e1206558064a459ca490a14fb734daecda3bf9c
shibdib <supernfadmin@gmail.com>
2017-05-20 04:18:16 -0400

    fix energy finding

--- inUse/role.basicHauler.js
+++ inUse/role.basicHauler.js
@@ -3,13 +3,8 @@ var roleBasicHauler = {
     /** @param {Creep} creep **/
     run: function (creep) {
         if (creep.carry.energy < creep.carryCapacity) {
-            if (creep.memory.energyTarget && Game.getObjectById(creep.memory.energyTarget)) {
-                var energy = creep.pos.findClosestByPath(creep.memory.energyTarget);
-            } else {
                 var energy = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY);
-            }
             if (energy) {
-                creep.memory.energyTarget = energy[0].id;
                 if (creep.pickup(energy[0]) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(energy[0], {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
