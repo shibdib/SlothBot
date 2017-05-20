696a91d479491b0e3643280a2b2e2a566492e663
shibdib <supernfadmin@gmail.com>
2017-05-20 06:20:42 -0400

    tweak construction queue

--- inUse/role.StationaryBuilder.js
+++ inUse/role.StationaryBuilder.js
@@ -47,10 +47,7 @@ module.exports = roleStationaryBuilder;
 
 function findConstruction(creep) {
 
-    site = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES, {filter: (s) => s.structureType === STRUCTURE_CONTAINER});
-    if (site === null) {
-        site = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES, {filter: (s) => s.structureType === STRUCTURE_EXTENSION});
-    }
+    site = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES, {filter: (s) => s.structureType === STRUCTURE_CONTAINER || s.structureType === STRUCTURE_EXTENSION});
     if (site === null) {
         site = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES, {filter: (s) => s.structureType !== STRUCTURE_RAMPART});
     }
