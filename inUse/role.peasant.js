919e2840a2836edb40bcf11058a9f40ec002b86d
shibdib <supernfadmin@gmail.com>
2017-05-20 19:10:01 -0400

    extension support for peasant

--- inUse/role.peasant.js
+++ inUse/role.peasant.js
@@ -12,13 +12,16 @@ let rolePeasant = {
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         } else {
-            if (creep.memory.spawnID && Game.getObjectById(creep.memory.spawnID)) {
-                var spawn = Game.getObjectById(creep.memory.spawnID);
-            } else {
-                var spawn = findSpawn(creep);
-            }
-            if (creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(spawn, {reusePath: 20}, {visualizePathStyle: {stroke: '#ffffff'}});
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType === STRUCTURE_EXTENSION ||
+                        structure.structureType === STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+                }
+            })
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {reusePath: 20}, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }
     }
