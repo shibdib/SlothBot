c534c32cdda1aba0f584d9c23f7dece7a136719b
shibdib <supernfadmin@gmail.com>
2017-05-21 04:15:39 -0400

    check if in the wrong room

--- inUse/module.borderChecks.js
+++ inUse/module.borderChecks.js
@@ -46,6 +46,7 @@ module.exports.isOnBorder = function(creep) {
 
 module.exports.wrongRoom = function(creep) {
     if(creep.memory.assignedSpawn === null) {
+        creep.say('1');
         let spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
         if (spawn) {
             creep.memory.assignedSpawn = spawn.id;
