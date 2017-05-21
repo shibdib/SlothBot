c3d36eb50420a3894735497cdc93ce67bcacddaa
shibdib <supernfadmin@gmail.com>
2017-05-20 21:42:46 -0400

    check if stuck on border

--- inUse/module.borderChecks.js
+++ inUse/module.borderChecks.js
@@ -1,40 +1,45 @@
-var nextStepIntoRoom = {
-    run: function(creep) {
-    if (creep.pos.x == 0 && creep.pos.y == 0) {
+module.exports.nextStepIntoRoom = function(creep) {
+    if(creep.pos.x == 0 && creep.pos.y == 0)
+    {
         creep.move(BOTTOM_RIGHT);
     }
-    else if (creep.pos.x == 0 && creep.pos.y == 49) {
+    else if(creep.pos.x == 0 && creep.pos.y == 49)
+    {
         creep.move(TOP_RIGHT);
     }
-    else if (creep.pos.x == 49 && creep.pos.y == 0) {
+    else if(creep.pos.x == 49 && creep.pos.y == 0)
+    {
         creep.move(BOTTOM_LEFT);
     }
-    else if (creep.pos.x == 49 && creep.pos.y == 49) {
+    else if(creep.pos.x == 49 && creep.pos.y == 49)
+    {
         creep.move(TOP_LEFT);
     }
-    else if (creep.pos.x == 49) {
+    else if(creep.pos.x == 49)
+    {
         creep.move(LEFT);
     }
-    else if (creep.pos.x == 0) {
+    else if (creep.pos.x == 0)
+    {
         creep.move(RIGHT);
     }
-    else if (creep.pos.y == 0) {
+    else if (creep.pos.y == 0)
+    {
         creep.move(BOTTOM);
     }
-    else if (creep.pos.y == 49) {
+    else if (creep.pos.y == 49)
+    {
         creep.move(TOP);
     }
 }
-};
-module.exports = nextStepIntoRoom;
 
-var isOnBorder = { run: function(creep) {
-    if (creep.pos.x == 0 || creep.pos.y == 0 || creep.pos.x == 49 || creep.pos.y == 49) {
+module.exports.IsOnBorder = function(creep) {
+    if(creep.pos.x == 0 || creep.pos.y == 0 || creep.pos.x == 49 || creep.pos.y == 49)
+    {
         return true;
     }
-    else {
+    else
+    {
         return false;
     }
-}
 };
-module.exports = isOnBorder;
