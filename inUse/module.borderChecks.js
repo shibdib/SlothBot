cc7e46ab030786a7a42a1f635aa6b2dd641a43fb
shibdib <supernfadmin@gmail.com>
2017-05-20 21:45:03 -0400

    check if stuck on border

--- inUse/module.borderChecks.js
+++ inUse/module.borderChecks.js
@@ -33,7 +33,7 @@ module.exports.nextStepIntoRoom = function(creep) {
     }
 }
 
-module.exports.IsOnBorder = function(creep) {
+module.exports.isOnBorder = function(creep) {
     if(creep.pos.x == 0 || creep.pos.y == 0 || creep.pos.x == 49 || creep.pos.y == 49)
     {
         return true;
