10117bd70a7f81f7d37f8cad57ee8dfa25f2333a
shibdib <supernfadmin@gmail.com>
2017-05-21 20:43:53 -0400

    tweak builders

--- inUse/roles.Workers.js
+++ inUse/roles.Workers.js
@@ -207,11 +207,11 @@ module.exports.Builder = function (creep) {
                 }
             }
         }
-    }
-    if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(creep.room.controller, {reusePath: 20}, {
-            visualizePathStyle: {stroke: '#ffffff'}
-        });
+        if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller, {reusePath: 20}, {
+                visualizePathStyle: {stroke: '#ffffff'}
+            });
+        }
     }
 };
 
