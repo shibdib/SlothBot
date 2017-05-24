690f929dafb3ea57cf562749e07cbae6e6465444
shibdib <supernfadmin@gmail.com>
2017-05-24 00:13:15 -0400

    new pathfinding

--- inUse/module.pathFinder.js
+++ inUse/module.pathFinder.js
@@ -1,14 +1,21 @@
 
 module.exports.Move = function (creep, target) {
-    if (creep.memory.pathAge === null || undefined) {
+    if (creep.memory.pathAge === null || creep.memory.pathAge === undefined) {
         creep.memory.pathAge = 0;
     }
-    if (creep.memory.pathAge > 10) {
+    if (creep.memory.pathAge >= 10) {
         creep.memory.path = creep.room.findPath(creep.pos, target.pos, {
             maxOps: 20000, serialize: true
         });
         creep.moveByPath(creep.memory.path);
         creep.memory.pathAge = 0;
     }
-    creep.moveByPath(creep.memory.path);
+    creep.memory.pathAge++;
+    if (creep.moveByPath(creep.memory.path) !== OK) {
+        creep.memory.path = creep.room.findPath(creep.pos, target.pos, {
+            maxOps: 20000, serialize: true
+        });
+        creep.moveByPath(creep.memory.path);
+        creep.memory.pathAge = 0;
+    }
 };
