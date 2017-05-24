034961f8c84762c7f4e3a90c97f57f7b92422544
shibdib <supernfadmin@gmail.com>
2017-05-23 23:04:30 -0400

    new pathing

--- inUse/module.pathFinder.js
+++ inUse/module.pathFinder.js
@@ -1,7 +1,7 @@
 
 let functions = require('module.functions');
 module.exports.Move = function (creep, target) {
-    return creep.room.findPath(creep.pos, target, {
+    return creep.room.findPath(creep.pos, target.pos, {
         maxOps: 20000, serialize: true
     });
 };
