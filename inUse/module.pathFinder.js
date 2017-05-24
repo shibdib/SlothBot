f9283695e859afe5588af6c24b09ae3fb5f4641d
shibdib <supernfadmin@gmail.com>
2017-05-24 00:48:46 -0400

    add a pathfinding check for new desto

--- inUse/module.pathFinder.js
+++ inUse/module.pathFinder.js
@@ -3,12 +3,16 @@ module.exports.Move = function (creep, target) {
     if (creep.memory.pathAge === null || creep.memory.pathAge === undefined) {
         creep.memory.pathAge = 0;
     }
+    if (creep.memory.target !== target.pos) {
+        creep.memory.pathAge = 11;
+    }
     if (creep.memory.pathAge >= 10) {
         creep.memory.path = creep.room.findPath(creep.pos, target.pos, {
             maxOps: 20000, serialize: true
         });
         creep.moveByPath(creep.memory.path);
         creep.memory.pathAge = 0;
+        creep.memory.target = target.pos;
     }
     creep.memory.pathAge++;
     if (creep.moveByPath(creep.memory.path) !== OK) {
@@ -17,6 +21,7 @@ module.exports.Move = function (creep, target) {
         });
         creep.moveByPath(creep.memory.path);
         creep.memory.pathAge = 0;
+        creep.memory.target = target.pos;
     }
 };
 module.exports.AttackMove = function (creep, target) {
