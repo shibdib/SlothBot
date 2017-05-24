4bc0f3c1506c47343f375adf473d163432bd122c
shibdib <supernfadmin@gmail.com>
2017-05-24 04:01:56 -0400

    2 more move's

--- inUse/module.respawn.js
+++ inUse/module.respawn.js
@@ -120,7 +120,7 @@ const respawnCreeps = {
                         let attack = 'attack' + i;
                         if (Game.flags[attack]) {
                             let attackers = _.filter(Game.creeps, (creep) => creep.memory.attackTarget === Game.flags[attack] && creep.memory.role === 'attacker');
-                            if (attackers.length < (i * 2) && Game.spawns[spawnName].canCreateCreep([TOUGH, TOUGH, ATTACK, ATTACK, MOVE, MOVE], generatedNumber + 'attacker') === OK) {
+                            if (attackers.length < (i * 2) && Game.spawns[spawnName].canCreateCreep([TOUGH, TOUGH, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE], generatedNumber + 'attacker') === OK) {
                                 Game.spawns[spawnName].createCreep([TOUGH, TOUGH, ATTACK, ATTACK, MOVE, MOVE], generatedNumber+'attacker', {
                                     role: 'attacker',
                                     attackTarget: Game.flags[attack],
