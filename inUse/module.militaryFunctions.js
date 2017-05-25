99c2a4e2f8fe4c072ddece453d0b068c4dd00b5c
shibdib <supernfadmin@gmail.com>
2017-05-25 06:59:58 -0400

    fix enemy check

--- inUse/module.militaryFunctions.js
+++ inUse/module.militaryFunctions.js
@@ -95,8 +95,10 @@ module.exports.buildWalls = function (spawn) {
 
 module.exports.findDefensivePosition = function (creep) {
     let closestEnemy = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-    let bestRampart = closestEnemy.pos.findClosestByPath(FIND_STRUCTURES, {filter: (r) => r.structureType === STRUCTURE_RAMPART && r.pos.getRangeTo(creep.memory.assignedSpawn) <= 4 && !r.pos.lookFor(LOOK_CREEPS)});
-    pathing.Move(creep, bestRampart, 1, true);
+    if (closestEnemy) {
+        let bestRampart = closestEnemy.pos.findClosestByPath(FIND_STRUCTURES, {filter: (r) => r.structureType === STRUCTURE_RAMPART && r.pos.getRangeTo(creep.memory.assignedSpawn) <= 4 && !r.pos.lookFor(LOOK_CREEPS)});
+        pathing.Move(creep, bestRampart, 1, true);
+    }
 };
 
 module.exports.activateDefense = function (spawn, scout) {
