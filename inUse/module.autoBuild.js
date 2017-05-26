3138d103f52926fff92f6354dbd8923ad94302da
shibdib <supernfadmin@gmail.com>
2017-05-26 03:17:42 -0400

    tweak rcl3

--- inUse/module.autoBuild.js
+++ inUse/module.autoBuild.js
@@ -48,10 +48,10 @@ module.exports.rcl4 = function (spawnName) {
     rcl3Extensions(spawn);
 
     //RCL4 Extensions
-    rcl4Extensions(spawn);
+    //rcl4Extensions(spawn);
 
     //RCL4 Storage
-    rcl4Storage(spawn);
+    //rcl4Storage(spawn);
 };
 
 function roadsSpawn(spawn) {
@@ -106,30 +106,7 @@ function roadsSpawn(spawn) {
 }
 
 function rcl2Extensions(spawn) {
-    const pos = new RoomPosition(spawn.pos.x+1, spawn.pos.y-2, spawn.room.name);
-    if (pos.lookFor(LOOK_STRUCTURES).length === 0 && pos.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
-        pos.createConstructionSite(STRUCTURE_EXTENSION);
-    }
-    const pos2 = new RoomPosition(spawn.pos.x+1, spawn.pos.y+2, spawn.room.name);
-    if (pos2.lookFor(LOOK_STRUCTURES).length === 0 && pos2.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
-        pos2.createConstructionSite(STRUCTURE_EXTENSION);
-    }
-    const pos3 = new RoomPosition(spawn.pos.x-1, spawn.pos.y+2, spawn.room.name);
-    if (pos3.lookFor(LOOK_STRUCTURES).length === 0 && pos3.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
-        pos3.createConstructionSite(STRUCTURE_EXTENSION);
-    }
-    const pos4 = new RoomPosition(spawn.pos.x-1, spawn.pos.y-2, spawn.room.name);
-    if (pos4.lookFor(LOOK_STRUCTURES).length === 0 && pos4.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
-        pos4.createConstructionSite(STRUCTURE_EXTENSION);
-    }
-    const pos5 = new RoomPosition(spawn.pos.x+2, spawn.pos.y-1, spawn.room.name);
-    if (pos5.lookFor(LOOK_STRUCTURES).length === 0 && pos5.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
-        pos5.createConstructionSite(STRUCTURE_EXTENSION);
-    }
-}
-
-function rcl3Extensions(spawn) {
-    const pos = new RoomPosition(spawn.pos.x-2, spawn.pos.y+1, spawn.room.name);
+    const pos = new RoomPosition(spawn.pos.x-2, spawn.pos.y-1, spawn.room.name);
     if (pos.lookFor(LOOK_STRUCTURES).length === 0 && pos.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
         pos.createConstructionSite(STRUCTURE_EXTENSION);
     }
@@ -141,11 +118,34 @@ function rcl3Extensions(spawn) {
     if (pos3.lookFor(LOOK_STRUCTURES).length === 0 && pos3.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
         pos3.createConstructionSite(STRUCTURE_EXTENSION);
     }
-    const pos4 = new RoomPosition(spawn.pos.x-1, spawn.pos.y-3, spawn.room.name);
+    const pos4 = new RoomPosition(spawn.pos.x+2, spawn.pos.y-1, spawn.room.name);
     if (pos4.lookFor(LOOK_STRUCTURES).length === 0 && pos4.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
         pos4.createConstructionSite(STRUCTURE_EXTENSION);
     }
-    const pos5 = new RoomPosition(spawn.pos.x+1, spawn.pos.y-3, spawn.room.name);
+    const pos5 = new RoomPosition(spawn.pos.x-3, spawn.pos.y+1, spawn.room.name);
+    if (pos5.lookFor(LOOK_STRUCTURES).length === 0 && pos5.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
+        pos5.createConstructionSite(STRUCTURE_EXTENSION);
+    }
+}
+
+function rcl3Extensions(spawn) {
+    const pos = new RoomPosition(spawn.pos.x-3, spawn.pos.y-1, spawn.room.name);
+    if (pos.lookFor(LOOK_STRUCTURES).length === 0 && pos.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
+        pos.createConstructionSite(STRUCTURE_EXTENSION);
+    }
+    const pos2 = new RoomPosition(spawn.pos.x+3, spawn.pos.y+1, spawn.room.name);
+    if (pos2.lookFor(LOOK_STRUCTURES).length === 0 && pos2.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
+        pos2.createConstructionSite(STRUCTURE_EXTENSION);
+    }
+    const pos3 = new RoomPosition(spawn.pos.x+3, spawn.pos.y-1, spawn.room.name);
+    if (pos3.lookFor(LOOK_STRUCTURES).length === 0 && pos3.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
+        pos3.createConstructionSite(STRUCTURE_EXTENSION);
+    }
+    const pos4 = new RoomPosition(spawn.pos.x-3, spawn.pos.y-2, spawn.room.name);
+    if (pos4.lookFor(LOOK_STRUCTURES).length === 0 && pos4.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
+        pos4.createConstructionSite(STRUCTURE_EXTENSION);
+    }
+    const pos5 = new RoomPosition(spawn.pos.x-3, spawn.pos.y+2, spawn.room.name);
     if (pos5.lookFor(LOOK_STRUCTURES).length === 0 && pos5.lookFor(LOOK_CONSTRUCTION_SITES).length === 0) {
         pos5.createConstructionSite(STRUCTURE_EXTENSION);
     }
