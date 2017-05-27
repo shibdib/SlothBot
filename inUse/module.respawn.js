7d8857228a9164c717ee1be1ef30f5100cba481c
shibdib <supernfadmin@gmail.com>
2017-05-27 06:19:45 -0400

    rcl3 tweak

--- inUse/module.respawn.js
+++ inUse/module.respawn.js
@@ -206,15 +206,15 @@ module.exports.rcl3 = function (spawnName) {
                 const stationaryHarvester = _.filter(Game.creeps, (creep) => creep.memory.role === 'stationaryHarvester' && creep.room === Game.spawns[spawnName].room);
                 const creeps = _.filter(Game.creeps);
                 const sourceCount = Game.spawns[spawnName].room.find(FIND_SOURCES).length;
+                const peasant = _.filter(Game.creeps, (creep) => creep.memory.assignedSource === sources[i].id && creep.memory.role === 'peasant');
                 const sources = Game.spawns[spawnName].room.find(FIND_SOURCES);
                 const containers = Game.spawns[spawnName].room.find(FIND_STRUCTURES, {
                     filter: {structureType: STRUCTURE_CONTAINER}
                 });
-                if (creeps.length <= 5 || Game.spawns[spawnName].room.energyCapacityAvailable < 800) {
+                if (creeps.length <= 5 || Game.spawns[spawnName].room.energyCapacityAvailable < 550 || peasant === 0) {
                     //PEASANT RESPAWNS
                     for (let i = 0; i < sources.length; i++) {
                         let harvester = _.filter(Game.creeps, (creep) => creep.memory.assignedSource === sources[i].id && creep.memory.role === 'stationaryHarvester');
-                        let peasant = _.filter(Game.creeps, (creep) => creep.memory.assignedSource === sources[i].id && creep.memory.role === 'peasant');
                         if (peasant.length === 0 && harvester.length === 0 && Game.spawns[spawnName].canCreateCreep([MOVE,MOVE,MOVE,WORK,WORK,WORK,CARRY,CARRY], generatedNumber + 'peasant') === OK) {
                             Game.spawns[spawnName].createCreep([MOVE,MOVE,MOVE,WORK,WORK,WORK,CARRY,CARRY], generatedNumber + 'peasant', {
                                 role: 'peasant',
