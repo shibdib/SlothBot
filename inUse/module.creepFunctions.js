a2ac3cdaeb149c2e01f36d98b8044a7af737384e
shibdib <supernfadmin@gmail.com>
2017-05-28 09:52:23 -0400

    tweak weight

--- inUse/module.creepFunctions.js
+++ inUse/module.creepFunctions.js
@@ -414,7 +414,7 @@ module.exports.findStorage = function (creep) {
     let tower = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_TOWER && s.energy < s.energyCapacity});
     if (tower) {
         const towerAmountWeighted = 1 - (tower.energy / tower.energyCapacity);
-        const towerDistWeighted = (tower.pos.getRangeTo(creep) * 1) - towerAmountWeighted;
+        const towerDistWeighted = (tower.pos.getRangeTo(creep) * 1.1) - towerAmountWeighted;
         storage.push({
             id: tower.id,
             distWeighted: towerDistWeighted
