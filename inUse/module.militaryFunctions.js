c76d047ec6c873f4e56af46a7d91941b114dd986
shibdib <supernfadmin@gmail.com>
2017-05-28 10:16:38 -0400

    fix wall code

--- inUse/module.militaryFunctions.js
+++ inUse/module.militaryFunctions.js
@@ -34,10 +34,8 @@ module.exports.buildWalls = function (spawn) {
         const buildWalls = build.findInRange(FIND_CONSTRUCTION_SITES, 1, {filter: (r) => r.structureType === STRUCTURE_WALL});
         const roadCheck = build.lookFor(LOOK_STRUCTURES);
         const constructionCheck = build.lookFor(LOOK_CONSTRUCTION_SITES);
-        if (roadCheck) {
-            if (roadCheck[0].structureType === STRUCTURE_WALL) {
-                spawn.memory.wallCheck = false;
-            }
+        if (roadCheck.length > 0 && roadCheck[0].structureType === STRUCTURE_WALL) {
+            spawn.memory.wallCheck = false;
         } else if (constructionCheck.length > 0) {
             spawn.memory.wallCheck = false;
         } else if (roadCheck.length > 0 && (roadCheck[0].structureType !== STRUCTURE_WALL || roadCheck[0].structureType !== STRUCTURE_RAMPART)) {
@@ -127,10 +125,8 @@ module.exports.borderWalls = function (spawn) {
                 const buildWalls = build.findInRange(FIND_CONSTRUCTION_SITES, 1, {filter: (r) => r.structureType === STRUCTURE_WALL});
                 const roadCheck = build.lookFor(LOOK_STRUCTURES);
                 const constructionCheck = build.lookFor(LOOK_CONSTRUCTION_SITES);
-                if (roadCheck[0]) {
-                    if (roadCheck[0].structureType === STRUCTURE_WALL) {
-                        spawn.memory.wallCheck = false;
-                    }
+                if (roadCheck.length > 0 && roadCheck[0].structureType === STRUCTURE_WALL) {
+                    spawn.memory.wallCheck = false;
                 } else if (constructionCheck.length > 0) {
                     spawn.memory.wallCheck = false;
                 } else if (roadCheck.length > 0 && (roadCheck[0].structureType !== STRUCTURE_WALL || roadCheck[0].structureType !== STRUCTURE_RAMPART)) {
@@ -169,10 +165,8 @@ module.exports.borderWalls = function (spawn) {
                 const buildWalls = build.findInRange(FIND_CONSTRUCTION_SITES, 1, {filter: (r) => r.structureType === STRUCTURE_WALL});
                 const roadCheck = build.lookFor(LOOK_STRUCTURES);
                 const constructionCheck = build.lookFor(LOOK_CONSTRUCTION_SITES);
-                if (roadCheck[0]) {
-                    if (roadCheck[0].structureType === STRUCTURE_WALL) {
-                        spawn.memory.wallCheck = false;
-                    }
+                if (roadCheck.length > 0 && roadCheck[0].structureType === STRUCTURE_WALL) {
+                    spawn.memory.wallCheck = false;
                 } else if (constructionCheck.length > 0) {
                     spawn.memory.wallCheck = false;
                 } else if (roadCheck.length > 0 && (roadCheck[0].structureType !== STRUCTURE_WALL || roadCheck[0].structureType !== STRUCTURE_RAMPART)) {
@@ -211,10 +205,8 @@ module.exports.borderWalls = function (spawn) {
                 const buildWalls = build.findInRange(FIND_CONSTRUCTION_SITES, 1, {filter: (r) => r.structureType === STRUCTURE_WALL});
                 const roadCheck = build.lookFor(LOOK_STRUCTURES);
                 const constructionCheck = build.lookFor(LOOK_CONSTRUCTION_SITES);
-                if (roadCheck[0]) {
-                    if (roadCheck[0].structureType === STRUCTURE_WALL) {
-                        spawn.memory.wallCheck = false;
-                    }
+                if (roadCheck.length > 0 && roadCheck[0].structureType === STRUCTURE_WALL) {
+                    spawn.memory.wallCheck = false;
                 } else if (constructionCheck.length > 0) {
                     spawn.memory.wallCheck = false;
                 } else if (roadCheck.length > 0 && (roadCheck[0].structureType !== STRUCTURE_WALL || roadCheck[0].structureType !== STRUCTURE_RAMPART)) {
@@ -253,10 +245,8 @@ module.exports.borderWalls = function (spawn) {
                 const buildWalls = build.findInRange(FIND_CONSTRUCTION_SITES, 1, {filter: (r) => r.structureType === STRUCTURE_WALL});
                 const roadCheck = build.lookFor(LOOK_STRUCTURES);
                 const constructionCheck = build.lookFor(LOOK_CONSTRUCTION_SITES);
-                if (roadCheck[0]) {
-                    if (roadCheck[0].structureType === STRUCTURE_WALL) {
-                        spawn.memory.wallCheck = false;
-                    }
+                if (roadCheck.length > 0 && roadCheck[0].structureType === STRUCTURE_WALL) {
+                    spawn.memory.wallCheck = false;
                 } else if (constructionCheck.length > 0) {
                     spawn.memory.wallCheck = false;
                 } else if (roadCheck.length > 0 && (roadCheck[0].structureType !== STRUCTURE_WALL || roadCheck[0].structureType !== STRUCTURE_RAMPART)) {
