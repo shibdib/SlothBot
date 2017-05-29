2998ac41e15b8b7ed668b48758901bd50c619afa
shibdib <supernfadmin@gmail.com>
2017-05-29 06:40:33 -0400

    fix brackets

--- inUse/module.respawn.js
+++ inUse/module.respawn.js
@@ -365,15 +365,15 @@ module.exports.rcl3 = function (spawnName) {
                             }
                         }
                     }
-
-                } else if (Game.spawns[spawnName].spawning) {
-                    let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name];
-                    Game.spawns[spawnName].room.visual.text(
-                        spawningCreep.memory.role,
-                        Game.spawns[spawnName].pos.x + 1,
-                        Game.spawns[spawnName].pos.y,
-                        {align: 'left', opacity: 0.8});
                 }
+
+            } else if (Game.spawns[spawnName].spawning) {
+                let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name];
+                Game.spawns[spawnName].room.visual.text(
+                    spawningCreep.memory.role,
+                    Game.spawns[spawnName].pos.x + 1,
+                    Game.spawns[spawnName].pos.y,
+                    {align: 'left', opacity: 0.8});
             }
         }
     }
@@ -578,15 +578,15 @@ module.exports.rcl4 = function (spawnName) {
                             }
                         }
                     }
-
-                } else if (Game.spawns[spawnName].spawning) {
-                    let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name];
-                    Game.spawns[spawnName].room.visual.text(
-                        spawningCreep.memory.role,
-                        Game.spawns[spawnName].pos.x + 1,
-                        Game.spawns[spawnName].pos.y,
-                        {align: 'left', opacity: 0.8});
                 }
+
+            } else if (Game.spawns[spawnName].spawning) {
+                let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name];
+                Game.spawns[spawnName].room.visual.text(
+                    spawningCreep.memory.role,
+                    Game.spawns[spawnName].pos.x + 1,
+                    Game.spawns[spawnName].pos.y,
+                    {align: 'left', opacity: 0.8});
             }
         }
     }
