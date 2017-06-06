f5868af48e031cff0e95c3fe3752c16a4e4a1cb5
shibdib <supernfadmin@gmail.com>
2017-06-06 04:55:52 -0400

    tweak

--- inUse/module.linkController.js
+++ inUse/module.linkController.js
@@ -6,24 +6,24 @@
 module.exports.linkControl = function () {
     for (let link of _.values(Game.structures)) {
         if (link.structureType === STRUCTURE_LINK) {
-            if (link.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType === STRUCTURE_STORAGE})) {
+            if (link.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType === STRUCTURE_STORAGE}).length > 0) {
                 link.room.memory.storageLink = link.id;
                 continue;
             }
-            if (link.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType === STRUCTURE_SPAWN})) {
+            if (link.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType === STRUCTURE_SPAWN}).length > 0) {
                 link.room.memory.spawnLink = link.id;
                 continue;
             }
             if (link.room.memory.storageLink) {
                 let storageLink = Game.getObjectById(link.room.memory.storageLink);
-                link.transfer(storageLink);
+                link.transferEnergy(storageLink);
                 if (link.energy === 0) {
                     continue;
                 }
             }
             if (link.room.memory.spawnLink) {
                 let spawnLink = Game.getObjectById(link.room.memory.spawnLink);
-                link.transfer(spawnLink);
+                link.transferEnergy(spawnLink);
             }
         }
     }
