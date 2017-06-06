6fafa83a197376bb1143b63d7c10f025c441e42d
shibdib <supernfadmin@gmail.com>
2017-06-06 06:59:50 -0400

    container first

--- inUse/module.creepFunctions.js
+++ inUse/module.creepFunctions.js
@@ -186,19 +186,22 @@ module.exports.containerBuilding = function (creep) {
 module.exports.harvestDeposit = function (creep) {
     let link = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_LINK && s.energy !== s.energyCapacity});
     let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_CONTAINER});
-    if (link) {
-        if (creep.pos.getRangeTo(link) <= 1) {
-            return link.id;
-        } else if (creep.pos.getRangeTo(link) <= 3) {
-            pathing.Move(creep, link);
-            return link.id;
+    if (container) {
+        if (container.store[RESOURCE_ENERGY] !== container.storeCapacity) {
+            if (creep.pos.getRangeTo(container) <= 1) {
+                return container.id;
+            } else if (creep.pos.getRangeTo(container) <= 3) {
+                pathing.Move(creep, container);
+                return container.id;
+            }
+        } else if (link) {
+            if (creep.pos.getRangeTo(link) <= 1) {
+                return link.id;
+            } else if (creep.pos.getRangeTo(link) <= 3) {
+                pathing.Move(creep, link);
+                return link.id;
+            }
         }
-    }
-    if (creep.pos.getRangeTo(container) <= 1) {
-        return container.id;
-    } else if (creep.pos.getRangeTo(container) <= 3) {
-        pathing.Move(creep, container);
-        return container.id;
     }
     return null;
 };
