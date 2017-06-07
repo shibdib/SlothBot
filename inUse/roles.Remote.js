57fae6fa2e798ae8cd41ac97f47e7fda8b74aa2f
shibdib <supernfadmin@gmail.com>
2017-06-07 03:56:14 -0400

    fix bug

--- inUse/roles.Remote.js
+++ inUse/roles.Remote.js
@@ -157,7 +157,7 @@ module.exports.spawnBuilder = function (creep) {
         creep.memory.hauling = true;
     }
     if (creep.memory.hauling === false) {
-        if (creep.room.name === Game.spawns[creep.memory.resupply].pos.roomName) {
+        if (creep.room.name === Game.spawns[Game.getObjectById(creep.memory.assignedSpawn).name].pos.roomName) {
             if (creep.memory.energyDestination) {
                 creepTools.withdrawEnergy(creep);
                 return null;
