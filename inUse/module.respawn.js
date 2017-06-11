3ecc21cf375163f0d48c45a8d6923b1db720a77d
shibdib <supernfadmin@gmail.com>
2017-06-11 01:45:18 -0400

    response forces

--- inUse/module.respawn.js
+++ inUse/module.respawn.js
@@ -245,6 +245,22 @@ function rcl3(spawnName) {
                     }
                 }
 
+                //Defense Force Spawn
+                let assistNeeded = _.filter(Game.creeps, (creep) => creep.memory.invaderDetected === true);
+                if (assistNeeded.length > 0) {
+                    let responder = _.filter(Game.creeps, (creep) => creep.memory.responseTarget === assistNeeded[0].name && creep.memory.role === 'responder');
+                    if (responder.length === 0 && Game.spawns[spawnName].canCreateCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, HEAL], generatedNumber + 'responder') === OK) {
+                        Game.spawns[spawnName].createCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, HEAL], generatedNumber + 'responder', {
+                            role: 'responder',
+                            assignedSpawn: Game.spawns[spawnName].id,
+                            level: 3,
+                            responseTarget: assistNeeded[0].pos.roomName
+                        });
+                        console.log('Spawning a responder');
+                        return;
+                    }
+                }
+
                 //RAIDER RESPAWNS
                 if (stationaryHarvester.length >= sourceCount) {
                     for (let i = 0; i < 10; i++) {
@@ -500,6 +516,22 @@ function rcl4(spawnName) {
                                 return;
                             }
                         }
+                    }
+                }
+
+                //Defense Force Spawn
+                let assistNeeded = _.filter(Game.creeps, (creep) => creep.memory.invaderDetected === true);
+                if (assistNeeded.length > 0) {
+                    let responder = _.filter(Game.creeps, (creep) => creep.memory.responseTarget === assistNeeded[0].name && creep.memory.role === 'responder');
+                    if (responder.length === 0 && Game.spawns[spawnName].canCreateCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK, HEAL], generatedNumber + 'responder') === OK) {
+                        Game.spawns[spawnName].createCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK, HEAL], generatedNumber + 'responder', {
+                            role: 'responder',
+                            assignedSpawn: Game.spawns[spawnName].id,
+                            level: 4,
+                            responseTarget: assistNeeded[0].pos.roomName
+                        });
+                        console.log('Spawning a responder');
+                        return;
                     }
                 }
 
