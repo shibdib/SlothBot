b1d810aff36eea9529623fa977c61b920f9b9d66
shibdib <supernfadmin@gmail.com>
2017-06-13 03:38:57 -0400

    claimers and responders

--- inUse/module.respawn.js
+++ inUse/module.respawn.js
@@ -595,7 +595,7 @@ function rcl4(spawnName) {
                 if (stationaryHarvester.length >= sourceCount && worker.length > 0 && upgrader.length > 0) {
                     for (let i = 0; i < 10; i++) {
                         let reserve = 'reserve' + i;
-                        if (Game.flags[reserve] && pathing.FindPath(Game.spawns[spawnName].pos, Game.flags[reserve].pos, false, false, 5).length < 100) {
+                        if (Game.flags[reserve] && pathing.FindPath(Game.spawns[spawnName], Game.flags[reserve], false, false, 5).length < 100) {
                             let reserver = _.filter(Game.creeps, (creep) => creep.memory.destination === Game.flags[reserve].name && creep.memory.role === 'reserver');
                             if (reserver.length < 1 && Game.spawns[spawnName].canCreateCreep([CLAIM, CLAIM, MOVE, MOVE], generatedNumber + 'reserver') === OK) {
                                 Game.spawns[spawnName].createCreep([CLAIM, CLAIM, MOVE, MOVE], generatedNumber + 'reserver', {
