b4cb55304b9b8e36d791262d8816572230c40f86
shibdib <supernfadmin@gmail.com>
2017-06-21 19:42:26 -0400

    add terminal controller

--- inUse/module.terminalController.js
+++ inUse/module.terminalController.js
@@ -16,14 +16,14 @@ module.exports.terminalControl = function () {
 function fillBuyOrders(terminal) {
     if (terminal.store[RESOURCE_ENERGY] >= 500) {
         for (const resourceType in terminal.store) {
-            if (resourceType.amount > 1000 && resourceType !== RESOURCE_ENERGY) {
+            if (terminal.store[resourceType] > 1000 && resourceType !== RESOURCE_ENERGY) {
                 let buyOrders = Game.market.getAllOrders(order => order.resourceType === resourceType &&
                 order.type === ORDER_BUY && order.remainingAmount >= 1000 &&
                 Game.market.calcTransactionCost(1000, terminal.pos.roomName, order.roomName) < 500);
                 if (buyOrders.length > 0) {
                     for (let i = 0; i < buyOrders.length; i++) {
                         if (Game.market.deal(buyOrders.id, 1000, terminal.pos.roomName) === OK) {
-                            console.log('buyOrderFilled -' + buyOrders.id);
+                            console.log('buyOrderFilled - 1000' + resourceType + 'for' + buyOrders[i].price * 1000);
                         }
                     }
                 }
