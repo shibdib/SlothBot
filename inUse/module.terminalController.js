0867ca9ccea65422666783c0b67e9e49597f24b9
shibdib <supernfadmin@gmail.com>
2017-06-21 22:55:08 -0400

    fix order duplication

--- inUse/module.terminalController.js
+++ inUse/module.terminalController.js
@@ -46,15 +46,18 @@ function buyEnergy(terminal) {
     for (const resourceType in terminal.store) {
         if (terminal.store[resourceType] < 10000 && resourceType === RESOURCE_ENERGY) {
             for (let key in Game.market.orders) {
-                if (Game.market.orders[key].resourceType === resourceType && Game.market.orders[key].type === ORDER_BUY && Game.market.orders[key].remainingAmount < 10000) {
-                    if (Game.market.extendOrder(Game.market.orders[key].id, 10000 - Game.market.orders[key].remainingAmount) === OK) {
-                        console.log('Extended energy buy order ' + Game.market.orders[key].id + 'an additional' + 10000 - Game.market.orders[key].remainingAmount);
+                if (Game.market.orders[key].resourceType === resourceType && Game.market.orders[key].type === ORDER_BUY) {
+                    if (Game.market.orders[key].remainingAmount < 10000) {
+                        if (Game.market.extendOrder(Game.market.orders[key].id, 10000 - Game.market.orders[key].remainingAmount) === OK) {
+                            console.log('Extended energy buy order ' + Game.market.orders[key].id + 'an additional' + 10000 - Game.market.orders[key].remainingAmount);
+                        }
+                        break;
                     }
-                    break;
+                } else {
+                    if (Game.market.createOrder(ORDER_BUY, resourceType, 0.01, 10000, terminal.pos.roomName) === OK) {
+                        console.log('Created energy buy order');
+                    }
                 }
-            }
-            if (Game.market.createOrder(ORDER_BUY, resourceType, 0.01, 10000, terminal.pos.roomName) === OK) {
-                console.log('Created energy buy order');
             }
         }
     }
@@ -70,14 +73,15 @@ function placeSellOrders(terminal) {
                             console.log('Extended sell order ' + Game.market.orders[key].id + 'an additional' + terminal.store[resourceType]);
                         }
                         break;
-                    }
-                }
-                let sellOrder = _.min(Game.market.getAllOrders(order => order.resourceType === resourceType &&
-                order.type === ORDER_SELL && order.remainingAmount >= 1000 &&
-                Game.market.calcTransactionCost(terminal.store[resourceType], terminal.pos.roomName, order.roomName) <= 1000), 'price');
-                if (sellOrder.id) {
-                    if (Game.market.createOrder(ORDER_SELL, resourceType, sellOrder.price - 0.01, terminal.store[resourceType], terminal.pos.roomName) === OK) {
-                        console.log('sellOrderSetup - 1000 ' + resourceType + ' for ' + sellOrder.price - 0.01);
+                    } else {
+                        let sellOrder = _.min(Game.market.getAllOrders(order => order.resourceType === resourceType &&
+                        order.type === ORDER_SELL && order.remainingAmount >= 1000 &&
+                        Game.market.calcTransactionCost(terminal.store[resourceType], terminal.pos.roomName, order.roomName) <= 1000), 'price');
+                        if (sellOrder.id) {
+                            if (Game.market.createOrder(ORDER_SELL, resourceType, sellOrder.price - 0.01, terminal.store[resourceType], terminal.pos.roomName) === OK) {
+                                console.log('sellOrderSetup - 1000 ' + resourceType + ' for ' + sellOrder.price - 0.01);
+                            }
+                        }
                     }
                 }
             }
