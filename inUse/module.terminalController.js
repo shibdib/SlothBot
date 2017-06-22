f8dcb2e47788ade721f4f018b9a47d78202fb9bf
shibdib <supernfadmin@gmail.com>
2017-06-21 20:19:05 -0400

    buy energy to keep terminal at 2000

--- inUse/module.terminalController.js
+++ inUse/module.terminalController.js
@@ -6,6 +6,12 @@
 module.exports.terminalControl = function () {
     for (let terminal of _.values(Game.structures)) {
         if (terminal.structureType === STRUCTURE_TERMINAL) {
+            //if we have credits make sure we have energy
+            if (Game.market.credits >= 100 && terminal.store[RESOURCE_ENERGY] < 2000 && terminal.store[RESOURCE_ENERGY] > 500) {
+                buyEnergy(terminal);
+            }
+
+            //if low on credits fill buy orders
             if (Game.market.credits < 1000) {
                 fillBuyOrders(terminal);
             }
@@ -20,7 +26,7 @@ function fillBuyOrders(terminal) {
                 let buyOrder = _.max(Game.market.getAllOrders(order => order.resourceType === resourceType &&
                 order.type === ORDER_BUY && order.remainingAmount >= 1000 &&
                 Game.market.calcTransactionCost(1000, terminal.pos.roomName, order.roomName) <= 500), 'price');
-                if (buyOrder) {
+                if (buyOrder.id) {
                     if (Game.market.deal(buyOrder.id, 1000, terminal.pos.roomName) === OK) {
                         console.log('buyOrderFilled - 1000 ' + resourceType + ' for ' + buyOrder.price * 1000);
                     }
@@ -28,15 +34,33 @@ function fillBuyOrders(terminal) {
                     let buyOrder = _.max(Game.market.getAllOrders(order => order.resourceType === resourceType &&
                     order.type === ORDER_BUY && order.remainingAmount >= 1000 &&
                     Game.market.calcTransactionCost(1000, terminal.pos.roomName, order.roomName) <= 1000), 'price');
-                    if (buyOrder) {
+                    if (buyOrder.id) {
                         if (Game.market.deal(buyOrder.id, 1000, terminal.pos.roomName) === OK) {
                             console.log('buyOrderFilled - 1000 ' + resourceType + ' for ' + buyOrder.price * 1000);
-                            }
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+function buyEnergy(terminal) {
+    let sellOrder = _.min(Game.market.getAllOrders(order => order.resourceType === RESOURCE_ENERGY &&
+    order.type === ORDER_SELL &&
+    Game.market.calcTransactionCost(100 * order.price, terminal.pos.roomName, order.roomName) <= 500), 'price');
+    if (sellOrder.id) {
+        if (Game.market.deal(sellOrder.id, 100 * sellOrder.price, terminal.pos.roomName) === OK) {
+            console.log('energyPurchased -' + 100 * sellOrder.price - Game.market.calcTransactionCost(100 * sellOrder.price, terminal.pos.roomName, sellOrder.roomName) + ' for ' + sellOrder.price * 1000);
+        }
     } else {
-        console.log('Terminal needs energy');
+        let sellOrder = _.min(Game.market.getAllOrders(order => order.resourceType === RESOURCE_ENERGY &&
+        order.type === ORDER_SELL &&
+        Game.market.calcTransactionCost(100 * order.price, terminal.pos.roomName, order.roomName) <= 1000), 'price');
+        if (sellOrder.id) {
+            if (Game.market.deal(sellOrder.id, 100 * sellOrder.price, terminal.pos.roomName) === OK) {
+                console.log('energyPurchased -' + 100 * sellOrder.price - Game.market.calcTransactionCost(100 * sellOrder.price, terminal.pos.roomName, sellOrder.roomName) + ' for ' + sellOrder.price * 1000);
+            }
+        }
     }
 }
