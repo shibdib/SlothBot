735ffe2c148be8b77cd6beb7df961c4e50c40d44
shibdib <supernfadmin@gmail.com>
2017-06-22 02:58:16 -0400

    tweak stuck pathfinding

--- inUse/module.pathFinder.js
+++ inUse/module.pathFinder.js
@@ -104,6 +104,9 @@ module.exports.Move = function (creep, target, exempt = false, maxRooms = 1) {
         creep.memory.pathAge = 0;
         creep.memory.pathLimit = (creep.memory.path.length + 3) / 2;
     }
+    if (!creep.memory.pathPos.x){
+        creep.memory.pathPos = creep.pos;
+    }
     if (creep.memory.pathPos.x && creep.memory.pathPos.x === creep.pos.x && creep.memory.pathPos.y === creep.pos.y) {
         creep.memory.pathPosTime++;
     } else {
