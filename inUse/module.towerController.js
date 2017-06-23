9a6e6b0de8baf520068e21d253de3e7e6fe730ff
shibdib <supernfadmin@gmail.com>
2017-06-23 01:43:22 -0400

    tweak defence code

--- inUse/module.towerController.js
+++ inUse/module.towerController.js
@@ -16,20 +16,26 @@ module.exports.towerControl = function () {
     for (let tower of _.values(Game.structures)) {
         if (tower.structureType === STRUCTURE_TOWER) {
             const barriers = tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => (s.structureType === STRUCTURE_RAMPART || s.structureType === STRUCTURE_WALL) && s.hits < 500});
-            const road = tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_ROAD || s.structureType === STRUCTURE_CONTAINER && s.hits < s.hitsMax * 0.25});
+            const road = tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_ROAD || s.structureType === STRUCTURE_CONTAINER && s.hits < s.hitsMax/2});
             const woundedCreep = Game.getObjectById(findWounded(tower));
             const closestHostile = tower.pos.findClosestByRange(FIND_CREEPS, {filter: (s) => include(doNotAggress,s.owner['username']) === false});
-            if (barriers) {
-                tower.repair(barriers);
-            } else if (road) {
-                tower.repair(road);
-            } else
-            //Check if hostiles are in room
             if (closestHostile) {
                 tower.attack(closestHostile);
-            } else if (woundedCreep) {
+                continue;
+            }
+            if (woundedCreep) {
                 tower.heal(woundedCreep);
-            } else if (tower.energy > tower.energyCapacity * 0.75) {
+                continue;
+            }
+            if (barriers) {
+                tower.repair(barriers);
+                continue;
+            }
+            if (road) {
+                tower.repair(road);
+                continue;
+            }
+            if (tower.energy > tower.energyCapacity * 0.75) {
                 const closestDamagedStructure = Game.getObjectById(findRepair(tower));
                 if (closestDamagedStructure) {
                     tower.repair(closestDamagedStructure);
