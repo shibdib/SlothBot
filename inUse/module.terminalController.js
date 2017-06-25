85752f9c4ae04d8ea52f2061eab6a60bb3ad3569
shibdib <supernfadmin@gmail.com>
2017-06-25 02:56:32 -0400

    fix updating energy price

--- inUse/module.terminalController.js
+++ inUse/module.terminalController.js
@@ -290,6 +290,14 @@ function sellPriceUpdates(terminal, globalOrders, myOrders) {
                     order.type === ORDER_SELL && order.remainingAmount >= 10000 && order.roomName !== terminal.pos.roomName), "price");
                     let buyOrder = _.max(globalOrders.filter(order => order.resourceType === resourceType &&
                     order.type === ORDER_BUY && order.remainingAmount >= 10000 && order.roomName !== terminal.pos.roomName), 'price');
+                    if (resourceType === RESOURCE_ENERGY) {
+                        if (sellOrder.id && _.round(sellOrder.price, 2) !== _.round(myOrders[key].price, 2)) {
+                            if (Game.market.changeOrderPrice(myOrders[key].id, _.round(sellOrder.price, 2)) === OK) {
+                                console.log("<font color='#adff2f'>MARKET: Sell order price change " + myOrders[key].id + " new/old " + _.round(sellOrder.price, 2) + "/" + myOrders[key].price + " Resource - " + resourceType + "</font>");
+                            }
+                            continue resource;
+                        }
+                    }
                     if (sellOrder.id && _.round(sellOrder.price - 0.01, 2) !== _.round(myOrders[key].price, 2) && _.round(sellOrder.price - 0.01, 2) > _.round(buyOrder.price, 2) && sellOrder.price - 0.01 !== 0) {
                         if (Game.market.changeOrderPrice(myOrders[key].id, _.round((sellOrder.price - 0.01), 2)) === OK) {
                             console.log("<font color='#adff2f'>MARKET: Sell order price change " + myOrders[key].id + " new/old " + _.round((sellOrder.price - 0.01), 2) + "/" + myOrders[key].price + " Resource - " + resourceType + "</font>");
