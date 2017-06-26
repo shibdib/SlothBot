8803539187b8fa7f49a42dea3b748fb2f2b527e1
shibdib <supernfadmin@gmail.com>
2017-06-26 07:08:24 -0400

    redo responders

--- inUse/module.respawn.js
+++ inUse/module.respawn.js
@@ -1408,7 +1408,7 @@ function collapsePrevention(spawnName) {
 function remoteNeighborCheck(spawnName, remote) {
     let neighboringRooms = Game.map.describeExits(Game.spawns[spawnName].pos.roomName);
     for (let i = 0; i < 10; i++) {
-        if (neighboringRooms[i] === Game.rooms[remote].name) {
+        if (neighboringRooms[i] === remote.name) {
             return true;
         }
     }
