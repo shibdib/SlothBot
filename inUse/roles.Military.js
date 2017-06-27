b9f6162e2032a98e534a67f3a9ee16bf6bb66371
shibdib <supernfadmin@gmail.com>
2017-06-27 18:40:31 -0400

    tweak

--- inUse/roles.Military.js
+++ inUse/roles.Military.js
@@ -321,7 +321,7 @@ function responder(creep) {
         creep.heal(creep);
     }
 
-    if (creep.pos.roomName === Game.rooms[creep.memory.responseTarget].name) {
+    if (Game.rooms[creep.memory.responseTarget] && creep.pos.roomName === Game.rooms[creep.memory.responseTarget].name) {
         creep.memory.destinationReached = true;
     }
 
