f8d9acc1913deff6f79f3825ac05b2de69229b7d
shibdib <supernfadmin@gmail.com>
2017-06-28 15:11:52 -0400

    tweak

--- inUse/roles.Military.js
+++ inUse/roles.Military.js
@@ -140,7 +140,7 @@ function attacker(creep) {
             pathing.AttackMove(creep, armedHostile);
         }
     } else if (closestHostileTower) {
-        if (creep.attack(closestHostileTower) === ERR_NOT_IN_RANGE && creep.rangedAttack(closestHostileTower) === ERR_NOT_IN_RANGE) {
+        if (creep.attack(closestHostileTower) === ERR_NOT_IN_RANGE) {
             creep.rangedAttack(closestHostileTower);
             if (creep.hits < creep.hitsMax) {
                 creep.heal(creep);
@@ -148,7 +148,7 @@ function attacker(creep) {
             pathing.AttackMove(creep, closestHostileTower);
         }
     } else if (closestHostileSpawn) {
-        if (creep.attack(closestHostileSpawn) === ERR_NOT_IN_RANGE && creep.rangedAttack(closestHostileTower) === ERR_NOT_IN_RANGE) {
+        if (creep.attack(closestHostileSpawn) === ERR_NOT_IN_RANGE) {
             creep.rangedAttack(closestHostileSpawn);
             if (creep.hits < creep.hitsMax) {
                 creep.heal(creep);
@@ -156,7 +156,7 @@ function attacker(creep) {
             pathing.AttackMove(creep, closestHostileSpawn);
         }
     } else if (closestHostile && creep.pos.roomName === Game.flags[creep.memory.attackTarget].pos.roomName) {
-        if (creep.attack(closestHostile) === ERR_NOT_IN_RANGE && creep.rangedAttack(closestHostile) === ERR_NOT_IN_RANGE) {
+        if (creep.attack(closestHostile) === ERR_NOT_IN_RANGE) {
             creep.rangedAttack(closestHostile);
             if (creep.hits < creep.hitsMax) {
                 creep.heal(creep);
@@ -164,7 +164,7 @@ function attacker(creep) {
             pathing.AttackMove(creep, closestHostile);
         }
     } else if (hostileStructures && creep.pos.roomName === Game.flags[creep.memory.attackTarget].pos.roomName) {
-        if (creep.attack(hostileStructures) === ERR_NOT_IN_RANGE && creep.rangedAttack(hostileStructures) === ERR_NOT_IN_RANGE) {
+        if (creep.attack(hostileStructures) === ERR_NOT_IN_RANGE) {
             creep.rangedAttack(hostileStructures);
             if (creep.hits < creep.hitsMax) {
                 creep.heal(creep);
