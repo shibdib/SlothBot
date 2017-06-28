b0f4a80b10122d75429df844b8ec95ddb586cce7
shibdib <supernfadmin@gmail.com>
2017-06-28 07:44:55 -0400

    tweak

--- inUse/module.towerController.js
+++ inUse/module.towerController.js
@@ -12,7 +12,7 @@ module.exports.towerControl = function () {
             const barriers = tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => (s.structureType === STRUCTURE_RAMPART || s.structureType === STRUCTURE_WALL) && s.hits < 500});
             const road = tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => (s.structureType === STRUCTURE_ROAD || s.structureType === STRUCTURE_CONTAINER) && s.hits < s.hitsMax * 0.25});
             const woundedCreep = tower.pos.findClosestByRange(FIND_CREEPS, {filter: (c) => c.hits < c.hitsMax && _.includes(doNotAggress, c.owner['username']) === true});
-            const closestHostile = tower.pos.findClosestByRange(FIND_CREEPS, {filter: (s) => _.includes(doNotAggress, s.owner['username']) === false});
+            const closestHostile = tower.pos.findInRange(FIND_CREEPS, 15, {filter: (s) => _.includes(doNotAggress, s.owner['username']) === false});
             if (closestHostile) {
                 tower.attack(closestHostile);
                 continue;
