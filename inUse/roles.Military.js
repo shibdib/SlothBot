a896225e41724cb8df4103c87a4b6c647bde8e48
shibdib <supernfadmin@gmail.com>
2017-06-28 00:17:44 -0400

    heal allies

--- inUse/roles.Military.js
+++ inUse/roles.Military.js
@@ -149,7 +149,7 @@ function attacker(creep) {
     } else {
         let nearbyAttackers = creep.pos.findInRange(attackers, 10);
         let nearbyHealers = creep.pos.findInRange(healers, 10);
-        if (nearbyAttackers.length < _.round(creep.memory.waitForAttackers / 2, 0) && nearbyHealers.length < _.round(creep.memory.waitForHealers / 2, 0)) {
+        if (nearbyAttackers.length < _.round(creep.memory.waitForAttackers / 2, 0) && nearbyHealers.length < creep.memory.waitForHealers) {
             creep.memory.attackStarted = false;
             return null;
         }
