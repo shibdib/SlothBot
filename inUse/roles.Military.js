10479c9b7713241c76b9d6c5edb49460193bb8a6
shibdib <supernfadmin@gmail.com>
2017-06-28 14:02:09 -0400

    tweak

--- inUse/roles.Military.js
+++ inUse/roles.Military.js
@@ -116,8 +116,9 @@ function attacker(creep) {
     }
     if (creep.memory.attackStarted !== true && Game.flags[creep.memory.staging].pos.roomName !== creep.pos.roomName) {
         pathing.Move(creep, Game.flags[creep.memory.staging], false, 16);
+        return null;
     }
-    
+
     let attackers = _.filter(Game.creeps, (a) => a.memory.attackTarget === creep.memory.attackTarget && a.memory.role === 'attacker');
     let healers = _.filter(Game.creeps, (h) => h.memory.attackTarget === creep.memory.attackTarget && h.memory.role === 'healer');
     let deconstructors = _.filter(Game.creeps, (h) => h.memory.attackTarget === creep.memory.attackTarget && h.memory.role === 'deconstructor');
