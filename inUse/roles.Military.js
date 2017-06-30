a6e7683b08b771dbcc55593c9f06c7c6f3fc6c4d
shibdib <supernfadmin@gmail.com>
2017-06-30 02:19:22 -0400

    tweak

--- inUse/roles.Military.js
+++ inUse/roles.Military.js
@@ -254,7 +254,7 @@ function ranged(creep) {
                     creep.heal(creep);
                 }
                 creep.travelTo(armedHostile, {allowHostile: true, range: 3});
-            } else if (creep.getRangeTo(armedHostile) < 3) {
+            } else if (creep.pos.getRangeTo(armedHostile) < 3) {
                 creep.travelTo(armedHostile, {allowHostile: true, range: 3, movingTarget: true});
             }
         } else if (closestHostileTower) {
