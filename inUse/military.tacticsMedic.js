2c398391b6f78ebb9fa15e1b29e946792e6d9d14
shibdib <supernfadmin@gmail.com>
2017-07-02 06:17:15 -0400

    tweak

--- inUse/military.tacticsMedic.js
+++ inUse/military.tacticsMedic.js
@@ -11,7 +11,7 @@ function inCombat(creep) {
     let squadLeader = _.filter(Game.creeps, (h) => h.memory.attackTarget === creep.memory.attackTarget && h.memory.squadLeader === true);
     let targets = creep.pos.findInRange(FIND_CREEPS, 3, {filter: (c) => c.hits < c.hitsMax && _.includes(doNotAggress, c.owner['username']) === true});
     let armedHostile = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS, {filter: (e) => (e.getActiveBodyparts(ATTACK) >= 1 || e.getActiveBodyparts(RANGED_ATTACK) >= 1) && _.includes(doNotAggress, e.owner['username']) === false});
-    if (!armedHostile || creep.pos.getRangeTo(armedHostile) >= 4) {
+    if (!armedHostile || creep.pos.getRangeTo(armedHostile) >= 6) {
         if (targets.length > 0) {
             if (creep.heal(targets[0]) === ERR_NOT_IN_RANGE) {
                 creep.rangedHeal(targets[0]);
@@ -28,7 +28,7 @@ function inCombat(creep) {
                 creep.rangedHeal(targets[0]);
             }
         }
-        militaryFunctions.kite(creep, 6);
+        militaryFunctions.kite(creep, 7);
     }
 }
 module.exports.inCombat = profiler.registerFN(inCombat, 'inCombatMedicTactic');
