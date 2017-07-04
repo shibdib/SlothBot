402fca5e03a778f69b4ad6f9a8a89903972c58e6
shibdib <supernfadmin@gmail.com>
2017-07-04 00:04:23 -0400

    tweak

--- inUse/military.tacticsDeconstructor.js
+++ inUse/military.tacticsDeconstructor.js
@@ -17,7 +17,7 @@ tacticSiege = function () {
         } else {
             this.siege();
         }
-    } else if (squadLeader[0].memory.attackStarted !== true) {
+    } else if (squadLeader[0].memory.attackStarted !== true || !squadLeader[0]) {
         this.travelTo(Game.flags[this.memory.staging]);
     } else {
         this.travelTo(Game.flags[this.memory.attackTarget], {allowHostile: false});
