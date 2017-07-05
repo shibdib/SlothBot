0af940b3cbec491e168ced9e1d26251ec8be7f27
shibdib <supernfadmin@gmail.com>
2017-07-05 10:26:17 -0400

    tweak

--- inUse/roles.Military.js
+++ inUse/roles.Military.js
@@ -43,7 +43,7 @@ function scout(creep) {
         if (creep.pos.getRangeTo(armedHostile) < 2) {
             militaryFunctions.kite(creep);
         }
-        this.travelTo(new RoomPosition(25, 25, creep.memory.destination), {range: 15});
+        creep.travelTo(new RoomPosition(25, 25, creep.memory.destination), {range: 15});
         if (creep.pos.roomName === creep.memory.destination) {
             creep.memory.destinationReached = true;
         }
