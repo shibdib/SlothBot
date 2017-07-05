a70ea5cb7bedaf71f32120090e580bfcd4ab3b0c
shibdib <supernfadmin@gmail.com>
2017-07-05 10:05:48 -0400

    tweak

--- inUse/military.tacticsRanged.js
+++ inUse/military.tacticsRanged.js
@@ -98,7 +98,7 @@ rangedTeam = function () {
                     this.travelTo(squadLeader[0], {allowHostile: true, movingTarget: true});
                 }
             }
-        } else if (hostiles.length > 0 && (!Game.flags[this.memory.attackTarget] || this.pos.roomName === Game.flags[this.memory.attackTarget].pos.roomName)) {
+        } else if (hostiles.length > 0 && (!this.memory.attackTarget || this.pos.roomName === this.memory.attackTarget)) {
             borderChecks.borderCheck(this);
             if ((closestHostileTower && this.pos.getRangeTo(closestHostileTower) < this.pos.getRangeTo(this.pos.findClosestByPath(hostiles))) || !closestHostileTower) {
                 if (inRangeHostile.length > 0) {
