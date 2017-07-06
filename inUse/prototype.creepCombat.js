dd620928169de2f8c2596f2d08417f5ba2d6518c
shibdib <supernfadmin@gmail.com>
2017-07-06 19:55:22 -0400

    tweak

--- inUse/prototype.creepCombat.js
+++ inUse/prototype.creepCombat.js
@@ -251,17 +251,6 @@ Creep.prototype.siege = function () {
             this.memory.siegeComplete = undefined;
         }
     }
-    if (target === null || (target.owner && _.includes(RawMemory.segments[2], target.owner['username']) === true)) {
-        if (!this.memory.siegeTarget || !Game.getObjectById(this.memory.siegeTarget)) {
-            target = _.min(this.pos.findInRange(FIND_HOSTILE_STRUCTURES, 25, {filter: (s) => (s.structureType === STRUCTURE_RAMPART || s.structureType === STRUCTURE_WALL) && _.includes(RawMemory.segments[2], s.owner['username']) === false}), 'hits');
-        } else {
-            target = Game.getObjectById(this.memory.siegeTarget);
-        }
-        if (target) {
-            this.memory.siegeTarget = target.id;
-            this.memory.siegeComplete = undefined;
-        }
-    }
     if (target === null) {
         let cs = this.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
         this.travelTo(cs);
