e34b455058f20716e3aa33307f2b875198e9bd2a
shibdib <supernfadmin@gmail.com>
2017-07-07 01:29:57 -0400

    tweak

--- inUse/prototype.creepCombat.js
+++ inUse/prototype.creepCombat.js
@@ -190,13 +190,13 @@ Creep.prototype.flee = function (target) {
 };
 
 Creep.prototype.fightRanged = function (target) {
-    if (this.hits < 0.5 * this.hitsMax) {
-        return this.flee(target);
+    if (this.hits < 0.7 * this.hitsMax) {
+        return this.kite();
     }
     let range = this.pos.getRangeTo(target);
     let direction = null;
     if (range <= 2) {
-        return this.flee(target);
+        return this.kite();
     }
     if (range <= 3) {
         let returnCode = this.rangedAttack(target);
@@ -410,6 +410,54 @@ Room.prototype.findAttackCreeps = function (object) {
     // TODO defender stop in rooms with (non attacking) enemies
     //    return false;
 };
+Creep.prototype.kite = function (fleeRange = 3) {
+    let avoid = this.room.find(FIND_HOSTILE_CREEPS, {filter: (c) => c.getActiveBodyparts(ATTACK) > 0 || c.getActiveBodyparts(RANGED_ATTACK) > 0});
+
+    let avoidance = _.map(this.pos.findInRange(avoid, fleeRange + 1),
+        (c) => {
+            return {pos: c.pos, range: 15};
+        });
+    let ret = PathFinder.search(this.pos, avoidance, {
+        flee: true,
+        swampCost: 50,
+        maxRooms: 1,
+
+        roomCallback: function (roomName) {
+            let costs = new PathFinder.CostMatrix;
+            addBorderToMatrix(this.room, costs);
+            return costs;
+        }
+
+    });
+
+    if (ret.path.length > 0) {
+        if (this.memory.squadLeader === true) {
+            this.memory.squadKite = this.pos.getDirectionTo(ret.path[0]);
+        }
+        return this.move(this.pos.getDirectionTo(ret.path[0]));
+    } else {
+        return OK;
+    }
+};
+
+function addBorderToMatrix(room, matrix) {
+    let exits = Game.map.describeExits(room.name);
+    if (exits === undefined) {
+        return matrix;
+    }
+    let top = ((_.get(exits, TOP, undefined) === undefined) ? 1 : 0);
+    let right = ((_.get(exits, RIGHT, undefined) === undefined) ? 48 : 49);
+    let bottom = ((_.get(exits, BOTTOM, undefined) === undefined) ? 48 : 49);
+    let left = ((_.get(exits, LEFT, undefined) === undefined) ? 1 : 0);
+    for (let y = top; y <= bottom; ++y) {
+        for (let x = left; x <= right; x += ((y % 49 === 0) ? 1 : 49)) {
+            if (matrix.get(x, y) < 0x03 && Game.map.getTerrainAt(x, y, room.name) !== "wall") {
+                matrix.set(x, y, 0x03);
+            }
+        }
+    }
+    return matrix;
+}
 
 Room.prototype.handleNukeAttack = function () {
     let nukes = this.find(FIND_NUKES);
