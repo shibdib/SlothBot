10bec981cb69e78e53c5747cc57467ed6d96f827
shibdib <supernfadmin@gmail.com>
2017-07-07 00:23:09 -0400

    tweak

--- inUse/military.tacticsMedic.js
+++ inUse/military.tacticsMedic.js
@@ -47,7 +47,7 @@ tacticSquadLeaderMedic = function () {
         } else if (this.memory.attackType !== 'siege' || siege.length > 0) {
             this.travelTo(new RoomPosition(25, 25, this.memory.attackTarget), {range: 24});
         } else if (this.memory.attackType === 'siege') {
-            this.travelTo(new RoomPosition(25, 25, this.memory.siegePoint), {range: 15});
+            this.travelTo(new RoomPosition(25, 25, this.memory.siegePoint), {range: 4});
         }
     } else {
         if (targets.length > 0) {
