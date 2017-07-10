e8d608ab939981bc6d058da102927d6ef824288d
shibdib <supernfadmin@gmail.com>
2017-07-10 00:36:11 -0400

    tweak

--- inUse/prototype.workerCreep.js
+++ inUse/prototype.workerCreep.js
@@ -715,3 +715,21 @@ findDeliveries = function () {
     }
 };
 Creep.prototype.findDeliveries = profiler.registerFN(findDeliveries, 'findDeliveriesCreepFunctions');
+
+/**
+ * Globally patch creep actions to log error codes.
+ */
+['attack', 'attackController', 'build', 'claimController', 'dismantle', 'drop',
+    'generateSafeMode', 'harvest', 'heal', 'move', 'moveByPath', 'moveTo', 'pickup',
+    'rangedAttack', 'rangedHeal', 'rangedMassAttack', 'repair', 'reserveController',
+    'signController', 'suicide', 'transfer', 'upgradeController', 'withdraw'].forEach(function (method) {
+    let original = Creep.prototype[method];
+    // Magic
+    Creep.prototype[method] = function () {
+        let status = original.apply(this, arguments);
+        if (typeof status === 'number' && status < 0) {
+            console.log(`Creep ${this.name} action ${method} failed with status ${status} at ${this.pos}`);
+        }
+        return status;
+    }
+});
