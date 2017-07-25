d4ff7492d1014ee4a89fc77193acea68eea4998f
shibdib <supernfadmin@gmail.com>
2017-07-25 09:31:34 -0400

    make links great again

--- inUse/module.linkController.js
+++ inUse/module.linkController.js
@@ -7,24 +7,14 @@ const profiler = require('screeps-profiler');
 function linkControl() {
     for (let link of _.values(Game.structures)) {
         if (link.structureType === STRUCTURE_LINK) {
-            if (link.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType === STRUCTURE_STORAGE}).length > 0) {
+            if (link.pos.findInRange(FIND_STRUCTURES, 3, {filter: (s) => s.structureType === STRUCTURE_STORAGE}).length > 0) {
                 link.room.memory.storageLink = link.id;
-                continue;
-            }
-            if (link.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType === STRUCTURE_SPAWN}).length > 0) {
-                link.room.memory.spawnLink = link.id;
                 continue;
             }
             if (link.room.memory.storageLink) {
                 let storageLink = Game.getObjectById(link.room.memory.storageLink);
-                if (storageLink.energy < 700) {
+                if (link.room.memory.storageLink !== link.id && storageLink.energy < 700) {
                     link.transferEnergy(storageLink);
-                }
-            }
-            if (link.room.memory.spawnLink) {
-                let spawnLink = Game.getObjectById(link.room.memory.spawnLink);
-                if (spawnLink.energy < 700) {
-                    link.transferEnergy(spawnLink);
                 }
             }
         }
--- inUse/role.RemoteHauler.js
+++ inUse/role.RemoteHauler.js
@@ -6,12 +6,13 @@ let _ = require('lodash');
 const profiler = require('screeps-profiler');
 
 function role(creep) {
+    if (creep.renewalCheck(4)) return null;
     //Invader detection
     if (!_.startsWith(creep.name, 'SK')) {
         invaderCheck(creep);
         if (creep.memory.invaderDetected === true || creep.memory.invaderCooldown < 50) {
             creep.memory.invaderCooldown++;
-            creep.shibMove(new RoomPosition(25, 25, creep.memory.assignedRoom));
+            creep.shibMove(new RoomPosition(25, 25, creep.memory.assignedRoom), {forceRepath: true});
             creep.memory.destinationReached = false;
             return null;
         } else if (creep.memory.invaderCooldown > 50) {
@@ -38,7 +39,7 @@ function role(creep) {
     if (creep.memory.destinationReached === true || creep.memory.hauling === true) {
         if (creep.memory.hauling === false) {
             if (!creep.memory.containerID) {
-                let container = creep.room.find(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_CONTAINER && _.sum(s.store) > s.storeCapacity / 2});
+                let container = creep.room.find(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_CONTAINER && _.sum(s.store) >= s.storeCapacity / 2});
                 if (container.length > 0) {
                     creep.memory.containerID = container[0].id;
                     for (const resourceType in container.store) {
@@ -67,25 +68,36 @@ function role(creep) {
         } else {
             if (creep.pos.roomName === creep.memory.assignedRoom) {
                 creep.memory.destinationReached = false;
-                let terminal = _.pluck(_.filter(creep.room.memory.structureCache, 'type', 'terminal'), 'id');
-                let storage = _.pluck(_.filter(creep.room.memory.structureCache, 'type', 'storage'), 'id');
-                if (storage.length > 0) {
-                    creep.memory.storageDestination = storage[0];
-                } else if (terminal.length > 0) {
-                    creep.memory.storageDestination = terminal[0];
-                }
-                if (creep.memory.storageDestination) {
-                    let storageItem = Game.getObjectById(creep.memory.storageDestination);
-                    for (const resourceType in creep.carry) {
-                        if (creep.transfer(storageItem, resourceType) === ERR_NOT_IN_RANGE) {
-                            creep.shibMove(storageItem);
+                let dropOffLink = Game.getObjectById(creep.memory.dropOffLink);
+                if (dropOffLink && creep.carry[RESOURCE_ENERGY] > 0) {
+                    if (creep.transfer(dropOffLink, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.shibMove(dropOffLink);
+                    }
+                } else {
+                    let link = creep.pos.findInRange(FIND_STRUCTURES, 6, {filter: (s) => s.structureType === STRUCTURE_LINK});
+                    if (link.length > 0 && link[0].id !== creep.room.memory.storageLink) {
+                        creep.memory.dropOffLink = link[0].id;
+                    }
+                    let terminal = _.pluck(_.filter(creep.room.memory.structureCache, 'type', 'terminal'), 'id');
+                    let storage = _.pluck(_.filter(creep.room.memory.structureCache, 'type', 'storage'), 'id');
+                    if (storage.length > 0) {
+                        creep.memory.storageDestination = storage[0];
+                    } else if (terminal.length > 0) {
+                        creep.memory.storageDestination = terminal[0];
+                    }
+                    if (creep.memory.storageDestination) {
+                        let storageItem = Game.getObjectById(creep.memory.storageDestination);
+                        for (const resourceType in creep.carry) {
+                            if (creep.transfer(storageItem, resourceType) === ERR_NOT_IN_RANGE) {
+                                creep.shibMove(storageItem);
+                            }
                         }
                     }
+                    creep.findStorage();
                 }
-                creep.findStorage();
             } else {
                 creep.shibMove(new RoomPosition(25, 25, creep.memory.assignedRoom), {
-                    range: 5
+                    range: 15
                 });
             }
         }
