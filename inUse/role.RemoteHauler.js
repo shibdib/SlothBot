0a6ee2dd6c2813f76f9b2289a5b167f8ecaaefd1
shibdib <supernfadmin@gmail.com>
2017-07-28 09:19:08 -0400

    disable renewing

--- inUse/role.RemoteHauler.js
+++ inUse/role.RemoteHauler.js
@@ -6,7 +6,7 @@ let _ = require('lodash');
 const profiler = require('screeps-profiler');
 
 function role(creep) {
-    if (creep.renewalCheck(4)) return creep.shibMove(Game.rooms[creep.memory.assignedRoom].find(FIND_MY_SPAWNS)[0]);
+    //if (creep.renewalCheck(4)) return creep.shibMove(Game.rooms[creep.memory.assignedRoom].find(FIND_MY_SPAWNS)[0]);
     //Invader detection
     if (!_.startsWith(creep.name, 'SK') && !creep.room.controller) {
         invaderCheck(creep);
