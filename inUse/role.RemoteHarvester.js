d28a7b7d67acaec617c76b28e6edc2085fbf7cbe
shibdib <supernfadmin@gmail.com>
2017-07-31 04:55:27 -0400

    stop remote mining in risings rooms

--- inUse/role.RemoteHarvester.js
+++ inUse/role.RemoteHarvester.js
@@ -22,9 +22,9 @@ function role(creep) {
             creep.memory.invaderCooldown = undefined;
         }
     }
-    
+
     //Mark room as no go if reserved or owned by someone else
-    if (creep.room.controller && (creep.room.controller.reservation.username !== 'Shibdib' || creep.room.owner)) {
+    if (creep.room.controller && ((creep.room.controller.reservation && creep.room.controller.reservation.username) !== 'Shibdib' || creep.room.owner)) {
         creep.room.memory.noRemote = true;
         creep.suicide();
     }
