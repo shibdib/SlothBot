acdd4c8600c17c40f6ace81347fa549c4558a2c2
shibdib <supernfadmin@gmail.com>
2017-08-06 04:58:14 -0400

    revert

--- inUse/role.SKSupport.js
+++ inUse/role.SKSupport.js
@@ -45,7 +45,7 @@ function role(creep) {
                     creep.shibMove(SKAttacker[0], {range: 0});
                     break;
                 case ERR_NOT_IN_RANGE:
-                    creep.shibMove(SKAttacker[0], {forceRepath: true, ignoreCreeps: false});
+                    creep.shibMove(SKAttacker[0], {forceRepath: true});
                     if (creep.hits < creep.hitsMax) creep.heal(creep);
                     break;
                 case ERR_NO_BODYPART:
