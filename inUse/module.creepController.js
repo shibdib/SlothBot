59ab99d8fa609df825d5a52712f6852564ba3dc6
shibdib <supernfadmin@gmail.com>
2017-08-08 18:01:43 -0400

    set non essential roles

--- inUse/module.creepController.js
+++ inUse/module.creepController.js
@@ -35,42 +35,37 @@ function creepControl() {
         if (creep.memory.role === 'drainer') roleDrainer.role(creep);
         if (creep.memory.role === 'swarm') roleSwarm.role(creep);
 
-        //Haulers
-        if (creep.memory.role === "pawn") {
-            let fillers = _.filter(Game.creeps, (c) => c.memory.role === 'filler' && c.memory.assignedRoom === creep.room.name);
-            if (fillers.length < 1) {
-                creep.memory.role = 'filler';
-                continue;
-            } else {
-                creep.memory.role = 'getter';
-                continue;
-            }
-        }
-        if (creep.memory.role === 'basicHauler') roleBasicHauler.role(creep);
-        if (creep.memory.role === 'getter') roleGetter.role(creep);
-        if (creep.memory.role === 'filler') roleFiller.role(creep);
-        if (creep.memory.role === 'mineralHauler') roleMineralHauler.role(creep);
-        if (creep.memory.role === 'labTech') roleLabTech.role(creep);
-        if (creep.memory.role === 'resupply') roleResupply.role(creep);
-
-        //Workers
-        if (creep.memory.role === 'worker') roleWorker.role(creep);
-        if (creep.memory.role === 'waller') roleWaller.role(creep);
-        if (creep.memory.role === 'upgrader') roleUpgrader.role(creep);
-        if (creep.memory.role === 'stationaryHarvester' || creep.memory.role === 'basicHarvester') roleHarvester.role(creep);
-        if (creep.memory.role === 'mineralHarvester') roleMineralHarvester.role(creep);
-        if (creep.memory.role === 'resupply') roleResupply.role(creep);
         //SK
-        if (creep.memory.role === 'SKworker') roleSKWorker.role(creep);
-        if (creep.memory.role === 'SKranged') roleSKRanged.role(creep);
         if (creep.memory.role === 'SKsupport') roleSKSupport.role(creep);
         if (creep.memory.role === 'SKattacker') roleSKAttacker.role(creep);
 
-        //Remotes
-        if (creep.memory.role === 'remoteHarvester') roleRemoteHarvester.role(creep);
-        if (creep.memory.role === 'remoteHauler') roleRemoteHauler.role(creep);
-        if (creep.memory.role === 'pioneer') rolePioneer.role(creep);
-        if (creep.memory.role === 'explorer') roleExplorer.role(creep);
+        //Non Essential
+        if (Game.cpu.getUsed() <= Game.cpu.limit * 0.90 && Game.cpu.bucket >= 1000) {
+            if (creep.memory.role === "pawn") {
+                let fillers = _.filter(Game.creeps, (c) => c.memory.role === 'filler' && c.memory.assignedRoom === creep.room.name);
+                if (fillers.length < 1) {
+                    creep.memory.role = 'filler';
+                    continue;
+                } else {
+                    creep.memory.role = 'getter';
+                    continue;
+                }
+            }
+            if (creep.memory.role === 'basicHauler') roleBasicHauler.role(creep);
+            if (creep.memory.role === 'resupply') roleResupply.role(creep);
+            if (creep.memory.role === 'getter') roleGetter.role(creep);
+            if (creep.memory.role === 'filler') roleFiller.role(creep);
+            if (creep.memory.role === 'worker') roleWorker.role(creep);
+            if (creep.memory.role === 'waller') roleWaller.role(creep);
+            if (creep.memory.role === 'upgrader') roleUpgrader.role(creep);
+            if (creep.memory.role === 'stationaryHarvester' || creep.memory.role === 'basicHarvester') roleHarvester.role(creep);
+            if (creep.memory.role === 'mineralHarvester') roleMineralHarvester.role(creep);
+            if (creep.memory.role === 'SKworker') roleSKWorker.role(creep);
+            if (creep.memory.role === 'remoteHarvester') roleRemoteHarvester.role(creep);
+            if (creep.memory.role === 'remoteHauler') roleRemoteHauler.role(creep);
+            if (creep.memory.role === 'pioneer') rolePioneer.role(creep);
+            if (creep.memory.role === 'explorer') roleExplorer.role(creep);
+        }
     }
 }
 
@@ -80,8 +75,6 @@ module.exports.creepControl = profiler.registerFN(creepControl, 'creepController
 let roleGetter = require('role.Getter');
 let roleFiller = require('role.Filler');
 let roleBasicHauler = require('role.BasicHauler');
-let roleLabTech = require('role.LabTech');
-let roleMineralHauler = require('role.MineralHauler');
 let roleResupply = require('role.Resupply');
 let roleWorker = require('role.Worker');
 let roleWaller = require('role.Waller');
@@ -89,7 +82,6 @@ let roleHarvester = require('role.Harvester');
 let roleMineralHarvester = require('role.MineralHarvester');
 let roleUpgrader = require('role.Upgrader');
 let roleSKWorker = require('role.SKWorker');
-let roleSKRanged = require('role.SKRanged');
 let roleSKSupport = require('role.SKSupport');
 let roleSKAttacker = require('role.SKAttacker');
 let roleRemoteHarvester = require('role.RemoteHarvester');
