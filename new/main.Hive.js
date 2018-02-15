8a882c73a80d561c10d22c40ff459d977075b493
shibdib <supernfadmin@gmail.com>
2018-02-15 15:26:00 -0500

    typo

--- new/main.Hive.js
+++ new/main.Hive.js
@@ -2,7 +2,7 @@ let profiler = require('screeps-profiler');
 let overlord = require('main.Overlord');
 
 function mind() {
-    Memory.ownedRooms = _.filter(Game.rooms, (r) => r.controller.owner && r.controller.owner['username'] === 'Shibdib');
+    Memory.ownedRooms = _.filter(Game.rooms, (r) => r.controller && r.controller.owner['username'] === 'Shibdib');
     let cpuBucket = Game.cpu.bucket;
 
     // Process Overlords
