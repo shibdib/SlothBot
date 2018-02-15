c291ab1a71c9898a0165a05edbbb7df0cfa66302
shibdib <supernfadmin@gmail.com>
2018-02-15 15:28:38 -0500

    typo

--- new/require.js
+++ new/require.js
@@ -2,5 +2,6 @@ require("globals")();
 require("prototype.workerCreep");
 require("prototype.roomPosition");
 require("prototype.room");
+require("prototype.roomBuilder");
 require("prototype.room.creepSpawning");
 require('module.pathFinder');
