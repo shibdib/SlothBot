28f0ca700a231aa4784b6248e1628b6a9506759a
shibdib <supernfadmin@gmail.com>
2018-02-17 06:04:56 -0500

    tweak worker count

--- new/prototype.room.creepSpawning.js
+++ new/prototype.room.creepSpawning.js
@@ -574,7 +574,7 @@ workerCreepQueue = function () {
     //Worker
     if (!_.includes(queue, 'worker')) {
         let workers = _.filter(roomCreeps, (creep) => creep.memory.role === 'worker');
-        if (workers.length < _.round((12 - level) / 2)) {
+        if (workers.length < _.round((9 - level) / 2)) {
             queueCreep(this, PRIORITIES.worker, {
                 role: 'worker'
             })
