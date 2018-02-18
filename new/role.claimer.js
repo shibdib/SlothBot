ae28fc53469dee67dd3e54225b8061b44b7f1d25
shibdib <supernfadmin@gmail.com>
2018-02-18 17:21:38 -0500

    tweak

--- new/role.claimer.js
+++ new/role.claimer.js
@@ -15,6 +15,11 @@ function role(creep) {
         }
     } else {
         if (creep.room.controller) {
+            if (!creep.memory.signed) {
+                let signs = ["Territory of Overlords - #overlords on Slack", "Overlords - Visit at your own risk.", "Join Overlords! #overlords"];
+                creep.signController(creep.room.controller, _.sample(signs));
+                creep.memory.signed = true;
+            }
             if (creep.claimController(creep.room.controller) === ERR_NOT_IN_RANGE) {
                 creep.shibMove(creep.room.controller);
             }
