ce8e8c8ac7b3265a9f3b68f45ab429df230f0b87
shibdib <supernfadmin@gmail.com>
2018-02-18 06:56:10 -0500

    tweak announce

--- new/role.upgrader.js
+++ new/role.upgrader.js
@@ -37,15 +37,18 @@ function role(creep) {
     }
     if (_.filter(Game.creeps, (c) => (c.memory.role === 'stationaryHarvester') && c.memory.overlord === creep.memory.overlord).length === 0) creep.memory.role = 'stationaryHarvester';
     //ANNOUNCE
-    let sentence = ['Spawn', 'More', 'Overlords', '#Overlords'];
-    if (creep.room.memory.claimTarget) {
-        sentence = ['Planned', 'Expansion:', creep.room.memory.claimTarget];
+    if (_.filter(Game.creeps, (c) => (c.memory.announcer === true) && c.memory.overlord === creep.memory.overlord).length === 0) creep.memory.announcer = true;
+    if (creep.memory.announcer) {
+        let sentence = ['~~', 'Spawn', 'More', 'Overlords', '#Overlords'];
+        if (creep.room.memory.claimTarget) {
+            sentence.concat(['~~', 'Planned', 'Expansion:', creep.room.memory.claimTarget]);
+        }
+        if (creep.room.memory.responseNeeded) {
+            sentence.concat(['~~', 'ROOM', 'SECURITY', 'ACTIVATED']);
+        }
+        let word = Game.time % sentence.length;
+        creep.say(sentence[word], true);
     }
-    if (creep.room.memory.responseNeeded) {
-        sentence = ['MILITARY', 'RESPONSE', 'REQUESTED'];
-    }
-    let word = Game.time % sentence.length;
-    creep.say(sentence[word], true);
     //INITIAL CHECKS
     if (creep.borderCheck()) return null;
     if (creep.wrongRoom()) return null;
