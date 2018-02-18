d6750877c2585a7f1543fae032cc5baa2b5b08ad
shibdib <supernfadmin@gmail.com>
2018-02-18 16:54:23 -0500

    tweak

--- new/prototype.workerCreep.js
+++ new/prototype.workerCreep.js
@@ -761,8 +761,8 @@ findEssentials = function () {
         let containerDistWeighted;
         const object = controllerContainer;
         let numberOfUsers = _.filter(Game.creeps, (c) => c.memory.energyDestination === object.id).length;
-        if (object && object.store[RESOURCE_ENERGY] < object.storeCapacity / 4 && numberOfUsers === 0) {
-            containerDistWeighted = _.round(object.pos.rangeToTarget(this) * 0.4, 0) + 1;
+        if (object && object.store[RESOURCE_ENERGY] < object.storeCapacity / 2 && numberOfUsers === 0) {
+            containerDistWeighted = _.round(object.pos.rangeToTarget(this) * 1 - (object.store[RESOURCE_ENERGY] / object.storeCapacity), 0) + 1;
         }
         storage.push({
             id: controllerContainer.id,
