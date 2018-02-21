5c84c43a6c57be6de6a8bc51406714c104d4e716
shibdib <supernfadmin@gmail.com>
2018-02-21 15:42:44 -0500

    tweak

--- new/prototype.room.expansion.js
+++ new/prototype.room.expansion.js
@@ -3,10 +3,10 @@ let profiler = require('screeps-profiler');
 claimNewRoom = function () {
     let avoidRooms = _.filter(Game.rooms, (r) => r.controller && r.controller.owner && _.includes(global.LOANlist, r.controller.owner['username']));
     let worthyRooms = _.filter(Memory.roomCache, (room) => room.claimWorthy && room.name !== this.name && room.sources.length === 2);
+    this.memory.claimTarget = undefined;
     if (avoidRooms.length === 0) return;
     if (worthyRooms.length > 0) {
         let possibles = {};
-        this.memory.claimTarget = undefined;
         loop1:
             for (let key in worthyRooms) {
                 let worthyName = worthyRooms[key].name;
