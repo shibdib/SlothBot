72f9ee803a6aba62761e8764cf4c6654b85491d9
shibdib <supernfadmin@gmail.com>
2018-02-21 15:34:26 -0500

    tweak

--- new/military.highCommand.js
+++ new/military.highCommand.js
@@ -8,11 +8,11 @@ function highCommand() {
     for (let key in Memory.ownedRooms) {
         let cleaningTargets = _.filter(Memory.roomCache, (r) => r.cached > Game.time - 1000 && r.needsCleaning && Game.map.findRoute(r.name, Memory.ownedRooms[key].name).length <= 5);
         if (cleaningTargets.length > 0) {
-            Game.rooms[Memory.ownedRooms[key]].memory.cleaningTargets = cleaningTargets;
+            Memory.ownedRooms[key].memory.cleaningTargets = cleaningTargets;
         }
         let localTargets = _.filter(Memory.roomCache, (r) => r.cached > Game.time - 1000 && r.owner && !_.includes(RawMemory.segments[2], constructionSite.owner['username']) && Game.map.findRoute(r.name, Memory.ownedRooms[key].name).length <= 3);
         if (localTargets.length > 0) {
-            Game.rooms[Memory.ownedRooms[key]].memory.localTargets = localTargets;
+            Memory.ownedRooms[key].memory.localTargets = localTargets;
         }
     }
 }
