54c16a78d0d258b77836dba66fe42ab5eece6b36
shibdib <supernfadmin@gmail.com>
2018-02-22 10:33:26 -0500

    tweak

--- new/module.linkController.js
+++ new/module.linkController.js
@@ -7,6 +7,7 @@ const profiler = require('screeps-profiler');
 function linkControl() {
     for (let link of _.values(Game.structures)) {
         if (link.structureType === STRUCTURE_LINK && link.id !== link.room.memory.controllerLink && link.id !== link.room.memory.storageLink && link.cooldown === 0) {
+            let energyPercentage = link.room.energyAvailable / link.room.energyCapacityAvailable;
             if (link.pos.findInRange(FIND_STRUCTURES, 3, {filter: (s) => s.structureType === STRUCTURE_STORAGE}).length > 0 && !Game.getObjectById(link.room.memory.storageLink)) {
                 link.room.memory.storageLink = link.id;
                 return;
@@ -20,7 +21,7 @@ function linkControl() {
                 if (!storageLink) link.room.memory.storageLink = undefined;
                 let controllerLink = Game.getObjectById(link.room.memory.controllerLink);
                 if (!controllerLink) link.room.memory.storageLink = undefined;
-                if (storageLink && storageLink.energy < 700 && (controllerLink && controllerLink.energy > 250)) {
+                if (storageLink && storageLink.energy < 700 && ((controllerLink && controllerLink.energy > 250) || (energyPercentage < 0.5))) {
                     link.transferEnergy(storageLink);
                 } else if (controllerLink && controllerLink.energy < 250) {
                     link.transferEnergy(controllerLink);
