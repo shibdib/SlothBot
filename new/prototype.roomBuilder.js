2791438a802bc7577764800ebb90727cbade8b16
shibdib <supernfadmin@gmail.com>
2018-02-24 06:43:18 -0500

    tweak

--- new/prototype.roomBuilder.js
+++ new/prototype.roomBuilder.js
@@ -127,6 +127,7 @@ function buildWalls(room, structures) {
         let position = new RoomPosition(safeZone[key].x, safeZone[key].y, room.name);
         if (position.getRangeTo(hub) === 6) {
             position.createConstructionSite(STRUCTURE_RAMPART);
+            position.createConstructionSite(STRUCTURE_ROAD);
         }
     }
 }
