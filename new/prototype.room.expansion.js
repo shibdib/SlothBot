a3ca1db4f1c66184b5d89fc30c993933d2aeccf2
shibdib <supernfadmin@gmail.com>
2018-02-25 02:15:14 -0500

    tweak

--- new/prototype.room.expansion.js
+++ new/prototype.room.expansion.js
@@ -3,6 +3,7 @@ let profiler = require('screeps-profiler');
 claimNewRoom = function () {
     let avoidRooms = _.filter(Game.rooms, (r) => r.controller && r.controller.owner && _.includes(FRIENDLIES, r.controller.owner['username']));
     let worthyRooms = _.filter(Memory.roomCache, (room) => room.claimWorthy && room.name !== this.name && room.sources.length === 2);
+    if (!Memory.lastExpansion) Memory.lastExpansion = Game.time;
     this.memory.claimTarget = undefined;
     if (avoidRooms.length === 0) return;
     if (worthyRooms.length > 0) {
@@ -10,7 +11,7 @@ claimNewRoom = function () {
         loop1:
             for (let key in worthyRooms) {
                 let worthyName = worthyRooms[key].name;
-                if (worthyRooms[key].owner || worthyRooms[key].reservation || 9 < Game.map.findRoute(this.name, worthyName).length || Game.map.getRoomLinearDistance(this.name, worthyName) > 4 || _.filter(Memory.ownedRooms, (room) => room.claimTarget && room.claimTarget === worthyName).length > 0) continue;
+                if (worthyRooms[key].owner || worthyRooms[key].reservation || 9 < Game.map.findRoute(this.name, worthyName).length || Game.map.getRoomLinearDistance(this.name, worthyName) > 6 || _.filter(Memory.ownedRooms, (room) => room.claimTarget && room.claimTarget === worthyName).length > 0) continue;
                 for (let key in avoidRooms) {
                     let name = avoidRooms[key].name;
                     let distance = Game.map.findRoute(worthyName, name).length;
@@ -23,6 +24,7 @@ claimNewRoom = function () {
         this.memory.claimTarget = _.pluck(_.sortBy(possibles, 'claimValue'), 'name')[0];
         if (this.memory.claimTarget) {
             Memory.roomCache[this.memory.claimTarget].claimWorthy = undefined;
+            Memory.lastExpansion = Game.time;
             Game.notify(this.memory.claimTarget + ' - Has been marked for claiming by ' + this.name);
             log.i(this.memory.claimTarget + ' - Has been marked for claiming by ' + this.name);
         } else {
