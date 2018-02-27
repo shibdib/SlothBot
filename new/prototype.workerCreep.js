4152eab6b4eacc9c1f8cd25960ea7e923f278d32
shibdib <supernfadmin@gmail.com>
2018-02-26 20:15:27 -0500

    structure/creep

--- new/prototype.workerCreep.js
+++ new/prototype.workerCreep.js
@@ -296,7 +296,7 @@ findEnergy = function (range = 250, hauler = false) {
     //storages
     //Storage
     let sStorage = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_STORAGE)[0];
-    if (sStorage && sStorage.store[RESOURCE_ENERGY] > 0) {
+    if (sStorage && sStorage.store[RESOURCE_ENERGY] > ENERGY_AMOUNT) {
         let weight;
         weight = 0.3;
         if (sStorage.store[RESOURCE_ENERGY] < 1000) {
@@ -420,14 +420,14 @@ getEnergy = function (range = 250, hauler = false) {
     }
     //Storage
     let sStorage = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_STORAGE)[0];
-    if (sStorage && sStorage.store[RESOURCE_ENERGY] > 0) {
+    if (sStorage && (sStorage.store[RESOURCE_ENERGY] > ENERGY_AMOUNT || this.room.memory.responseNeeded)) {
         let weight;
         weight = 0;
-        if (sStorage.store[RESOURCE_ENERGY] > 50000) {
+        if (sStorage.store[RESOURCE_ENERGY] > ENERGY_AMOUNT) {
             weight = 0.3;
         }
         if (this.room.memory.responseNeeded) {
-            weight = 0.8;
+            weight = 1.2;
         }
         if (sStorage.pos.getRangeTo(this) > 1) {
             const storageDistWeighted = _.round(sStorage.pos.rangeToTarget(this) * weight, 0) + 1;
