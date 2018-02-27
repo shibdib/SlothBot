86a26dc5f30b9ab331c1dfbb533ef8c40cee36d7
shibdib <supernfadmin@gmail.com>
2018-02-27 06:06:15 -0500

    mineral property

--- new/role.deconstructor.js
+++ new/role.deconstructor.js
@@ -6,23 +6,6 @@ let _ = require('lodash');
 const profiler = require('screeps-profiler');
 
 function role(creep) {
-    if (creep.memory.boostAttempt !== true) {
-        let desiredReactions = [];
-        let count = 1;
-        for (let i = 0; i < desiredReactions.length; i++) {
-            let lab = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_LAB && s.mineralType === desiredReactions[i] && s.mineralAmount >= 30 && s.energy >= 20});
-            if (lab) {
-                count++;
-                if (lab.boostCreep(creep) === ERR_NOT_IN_RANGE) {
-                    creep.shibMove(lab);
-                }
-            }
-        }
-        if (count === 1) {
-            creep.memory.boostAttempt = true;
-        }
-        return null;
-    }
     creep.borderCheck();
     if (creep.memory.operation === 'clean') return creep.cleanRoom();
 }
