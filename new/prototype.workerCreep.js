f56cb07756841fb0c6055d063b88bae519bcee5d
shibdib <supernfadmin@gmail.com>
2018-03-07 11:39:23 -0500

    tweak

--- new/prototype.workerCreep.js
+++ new/prototype.workerCreep.js
@@ -368,7 +368,7 @@ getEnergy = function (range = 250, hauler = false) {
     }
     //Storage
     let sStorage = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_STORAGE)[0];
-    if (sStorage && sStorage.store[RESOURCE_ENERGY] > ENERGY_AMOUNT || this.room.memory.responseNeeded && sStorage.pos.rangeToTarget(this) <= range) {
+    if (sStorage && sStorage.store[RESOURCE_ENERGY] > ENERGY_AMOUNT || this.room.memory.responseNeeded && 1 > sStorage.pos.rangeToTarget(this) <= range) {
         let weight = 0.3;
         if (this.room.memory.responseNeeded) weight = 1.2;
         const storageDistWeighted = _.round(sStorage.pos.rangeToTarget(this) * weight, 0) + 1;
