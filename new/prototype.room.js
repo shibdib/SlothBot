5901b72479ad172031efca59fbc0c7e71ac0e4bf
shibdib <supernfadmin@gmail.com>
2018-03-08 15:27:17 -0500

    tweak

--- new/prototype.room.js
+++ new/prototype.room.js
@@ -144,26 +144,6 @@ Object.defineProperty(Room.prototype, 'level', {
     configurable: true
 });
 
-/**
- * Provides structure memory.
- */
-Object.defineProperty(Structure.prototype, 'memory', {
-    get: function () {
-        if (Memory.structures === undefined) {
-            Memory.structures = {};
-        }
-        if (Memory.structures[this.id] === undefined) {
-            Memory.structures[this.id] = {};
-        }
-        return Memory.structures[this.id];
-    },
-    set: function (v) {
-        return _.set(Memory, 'structures.' + this.id, v);
-    },
-    configurable: true,
-    enumerable: false,
-});
-
 
 //Room Cache
 ///////////////////////////////////////////////////
