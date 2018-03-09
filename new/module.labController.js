e1edcf1879052df28f18073fa03e4851e9037897
shibdib <supernfadmin@gmail.com>
2018-03-09 04:29:19 -0500

    tweak

--- new/module.labController.js
+++ new/module.labController.js
@@ -37,15 +37,15 @@ function manageReactions(room) {
         let componentOne = BOOST_COMPONENTS[boost][0];
         let componentTwo = BOOST_COMPONENTS[boost][1];
         if (((storage.store[componentOne] || 0 + terminal.store[componentOne] || 0) > 500) && ((storage.store[componentTwo] || 0 + terminal.store[componentTwo] || 0) > 500)) {
-            let availableLabs = _.filter(room.structures, (s) => s.structureType === STRUCTURE_LAB && !Memory.structures[s.id].active && s.pos.findInRange(room.structures, 3, {filter: (l) => l.structureType === STRUCTURE_LAB && !Memory.structures[s.id].active}).length >= 2)[0];
+            let availableLabs = _.filter(room.structures, (s) => s.structureType === STRUCTURE_LAB && !s.memory.active && s.pos.findInRange(room.structures, 3, {filter: (l) => l.structureType === STRUCTURE_LAB && !l.memory.active}).length >= 2)[0];
             if (availableLabs) {
                 log.a(room.name + ' queued ' + boost + ' for creation.');
                 room.memory.activeReaction = boost;
-                let hub = availableLabs.pos.findInRange(room.structures, 3, {filter: (s) => s.structureType === STRUCTURE_LAB && !Memory.structures[s.id].active});
+                let hub = availableLabs.pos.findInRange(room.structures, 3, {filter: (s) => s.structureType === STRUCTURE_LAB && !s.memory.active});
                 for (let labID in hub) {
-                    let one = _.filter(hub, (h) => Memory.structures[h.id].itemNeeded === componentOne)[0];
-                    let two = _.filter(hub, (h) => Memory.structures[h.id].itemNeeded === componentTwo)[0];
-                    let out = _.filter(hub, (h) => Memory.structures[h.id].itemNeeded === boost)[0];
+                    let one = _.filter(hub, (h) => h.memory.itemNeeded === componentOne)[0];
+                    let two = _.filter(hub, (h) => h.memory.itemNeeded === componentTwo)[0];
+                    let out = _.filter(hub, (h) => h.memory.itemNeeded === boost)[0];
                     if (!one) {
                         Memory.structures[hub[labID].id] = {
                             itemNeeded: componentOne,
