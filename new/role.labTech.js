cfa9fbb0cb615a36d682ea13d5a5b3b45e7e06a1
shibdib <supernfadmin@gmail.com>
2018-03-09 07:17:33 -0500

    tweak

--- new/role.labTech.js
+++ new/role.labTech.js
@@ -10,7 +10,7 @@ const profiler = require('screeps-profiler');
  */
 function role(creep) {
     //INITIAL CHECKS
-    creep.say("U+1F52C", true);
+    creep.say("Lab Rat", true);
     if (creep.renewalCheck(6)) return null;
     if (creep.borderCheck()) return null;
     if (creep.wrongRoom()) return null;
@@ -75,7 +75,7 @@ function role(creep) {
                         }
                     }
                     if (creep.memory.itemStorage) {
-                        creep.say("U+1F52C", true);
+                        creep.say("Lab Rat", true);
                         creep.memory.storageDestination = labs[key].id;
                         switch (creep.withdraw(Game.getObjectById(creep.memory.itemStorage), labs[key].memory.itemNeeded)) {
                             case OK:
@@ -87,7 +87,7 @@ function role(creep) {
                         }
                     }
                 } else {
-                    creep.say("U+1F52C", true);
+                    creep.say("Lab Rat", true);
                     let lab = Game.getObjectById(creep.memory.labHelper);
                     if (lab) {
                         switch (creep.transfer(lab, labs[key].memory.itemNeeded)) {
