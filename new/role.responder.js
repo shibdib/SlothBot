6444acfb7c27ff71ce87a16c8c0025ba9b86fc2c
shibdib <supernfadmin@gmail.com>
2018-03-12 06:01:34 -0400

    tweak

--- new/role.responder.js
+++ new/role.responder.js
@@ -8,7 +8,7 @@ const profiler = require('screeps-profiler');
 function role(creep) {
     creep.say(ICONS.respond, true);
     creep.room.invaderCheck();
-    if (!creep.memory.boostAttempt) return creep.tryToBoost(['attack']);
+    if (creep.room.memory.threatLevel > 2) if (!creep.memory.boostAttempt) return creep.tryToBoost(['attack']);
     creep.borderCheck();
     if (!creep.handleDefender()) {
         findDefensivePosition(creep, creep);
