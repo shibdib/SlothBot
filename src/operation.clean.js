46c6c172dc358962b95137e5f4e58d3e6eba051b
shibdib <supernfadmin@gmail.com>
2018-03-14 10:12:45 -0400

    tweak

--- src/operation.clean.js
+++ src/operation.clean.js
@@ -11,7 +11,7 @@ Creep.prototype.cleanRoom = function () {
     }
     let target = this.pos.findClosestByPath(this.room.structures, {filter: (s) => s.structureType !== STRUCTURE_CONTROLLER && s.structureType !== STRUCTURE_STORAGE && s.structureType !== STRUCTURE_TERMINAL && s.structureType !== STRUCTURE_WALL && s.structureType !== STRUCTURE_RAMPART});
     if (!target || target === null) {
-        target = this.findClosestBarrier();
+        target = this.findClosestBarrier(false);
     }
     if (!target) {
         switch (this.signController(this.room.controller, 'Room cleaned courtesy of #overlords.')) {
--- src/prototype.creepCombat.js
+++ src/prototype.creepCombat.js
@@ -34,8 +34,13 @@ Creep.prototype.findClosestEnemy = function () {
     }
 };
 
-Creep.prototype.findClosestBarrier = function () {
-    let barriers = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_RAMPART);
+Creep.prototype.findClosestBarrier = function (walls = true) {
+    let barriers;
+    if (walls) {
+        barriers = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_RAMPART);
+    } else {
+        barriers = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_RAMPART);
+    }
     let lowestInArea = _.sortBy(this.pos.findInRange(barriers, 6), 'hits')[0];
     if (lowestInArea && !PathFinder.search(this.pos, lowestInArea.pos).incomplete) return lowestInArea;
     return this.pos.findClosestByRange(barriers);
