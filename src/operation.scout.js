e03aa6b1349fe553ed26ae61ec290d6ce63d15ac
shibdib <supernfadmin@gmail.com>
2018-03-17 03:31:57 -0400

    tweak

--- src/operation.scout.js
+++ src/operation.scout.js
@@ -1,8 +1,8 @@
 Creep.prototype.scoutRoom = function () {
     if (this.room.name !== this.memory.targetRoom) return this.shibMove(new RoomPosition(25, 25, this.memory.targetRoom), {range: 23});
     this.room.cacheRoomIntel(true);
-    let towers = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_TOWER && !_.includes(FRIENDLIES, s.owner.username));
-    let ramparts = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_RAMPART && !_.includes(FRIENDLIES, s.owner.username));
+    let towers = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_TOWER);
+    let ramparts = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_RAMPART);
     let terminal = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_TERMINAL)[0];
     let storage = _.filter(this.room.structures, (s) => s.structureType === STRUCTURE_STORAGE)[0];
     let controller = this.room.controller;
@@ -15,7 +15,7 @@ Creep.prototype.scoutRoom = function () {
             level: 2
         };
         Memory.targetRooms = cache;
-    } else if (controller.owner && (towers.length === 0 || _.max(towers, 'energy').energy === 0) && !ramparts[0] && (terminal && _.sum(terminal.store) > 0) || (storage && _.sum(storage.store) > 0)) {
+    } else if (controller.owner && (towers.length === 0 || _.max(towers, 'energy').energy === 0) && !ramparts[0] && ((terminal && _.sum(terminal.store) > 0) || (storage && _.sum(storage.store) > 0))) {
         let cache = Memory.targetRooms || {};
         let tick = Game.time;
         cache[this.pos.roomName] = {
@@ -24,7 +24,7 @@ Creep.prototype.scoutRoom = function () {
             level: 1
         };
         Memory.targetRooms = cache;
-    } else if (controller.owner && (towers.length >= 1)) {
+    } else if (controller.owner && towers.length >= 1) {
         let cache = Memory.targetRooms || {};
         let tick = Game.time;
         let level = _.round((towers.length / 2) + 0.5);
