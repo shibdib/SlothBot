87e9dcba2dbd228a9777b26c32cbfffc0121e6de
shibdib <supernfadmin@gmail.com>
2018-03-17 04:48:21 -0400

    tweak

--- src/module.terminalController.js
+++ src/module.terminalController.js
@@ -359,6 +359,13 @@ function orderCleanup(myOrders) {
                     }
                 }
             }
+            if (myOrders[key].resourceType !== RESOURCE_ENERGY) {
+                if (myOrders[key].amount < 250) {
+                    if (Game.market.cancelOrder(myOrders[key].id) === OK) {
+                        log.e(" MARKET: Order Cancelled: " + myOrders[key].id + " - Order Fulfilled.");
+                    }
+                }
+            }
         } else {
             if (myOrders[key].resourceType !== RESOURCE_ENERGY) {
                 if (myOrders[key].amount < 250) {
