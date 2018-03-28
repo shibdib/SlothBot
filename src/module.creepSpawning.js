50b00c7d651ef7778e6fe4de3998a600dfdc9a1a
shibdib <supernfadmin@gmail.com>
2018-03-28 18:59:01 -0400

    tweak

--- src/module.creepSpawning.js
+++ src/module.creepSpawning.js
@@ -758,6 +758,7 @@ function bodyGenerator(level, role) {
             if (level < 4) break;
             tough = _.round(0.5 * level);
             rangedAttack = (1 * level) + 1;
+            heal = 0;
             if (level >= 6) heal = 1;
             move = tough + rangedAttack + heal;
             break;
