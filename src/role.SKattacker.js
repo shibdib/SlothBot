5e89e99e8f50b330c4388ed0d18772bd2ba910c8
shibdib <supernfadmin@gmail.com>
2018-03-28 20:20:33 -0400

    tweak

--- src/role.SKattacker.js
+++ src/role.SKattacker.js
@@ -7,10 +7,7 @@ const profiler = require('screeps-profiler');
 
 function role(creep) {
     if (creep.memory.boostAttempt !== true) return creep.tryToBoost(['attack']);
-    if (creep.ticksToLive < 300) {
-        if (creep.room.name !== creep.memory.overlord) return creep.shibMove(new RoomPosition(25, 25, creep.memory.overlord), {range: 20});
-        if (creep.renewalCheck(1, 300, 1400, true)) return null;
-    }
+    if (creep.renewalCheck(5)) return null;
     if (creep.hits < creep.hitsMax) creep.heal(creep);
     if (!creep.memory.destination) creep.memory.destination = shuffle(creep.memory.misc)[0];
     if (creep.room.name === creep.memory.destination) {
