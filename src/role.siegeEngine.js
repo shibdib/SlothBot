fb2c3daaeacc632379be39ae4bf7b7f084aaa03e
shibdib <supernfadmin@gmail.com>
2018-04-01 17:53:01 -0400

    tweak

--- src/role.siegeEngine.js
+++ src/role.siegeEngine.js
@@ -8,6 +8,8 @@ const profiler = require('screeps-profiler');
 function role(creep) {
     if (!creep.memory.boostAttempt) return creep.tryToBoost(['tough', 'attack', 'ranged']);
     creep.borderCheck();
+    let alliedCreep = _.filter(creep.room.creeps, (c) => !c.my && _.includes(FRIENDLIES, c.owner));
+    if (!creep.pos.findInRange(alliedCreep, 3)[0] && creep.getActiveBodyparts(RANGED_ATTACK) > 0) creep.rangedMassAttack();
     creep.siegeRoom();
 }
 
