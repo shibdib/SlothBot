e4940be6d3f9b52dc46e82f8a0a6e9b70ce14791
shibdib <supernfadmin@gmail.com>
2018-04-06 14:17:59 -0400

    tweak

--- src/role.worker.js
+++ src/role.worker.js
@@ -53,8 +53,8 @@ function role(creep) {
                     }
                 } else if (creep.upgradeController(Game.rooms[creep.memory.overlord].controller) === ERR_NOT_IN_RANGE) {
                     creep.shibMove(Game.rooms[creep.memory.overlord].controller);
-                    this.memory.constructionSite = undefined;
-                    this.memory.task = undefined;
+                    creep.memory.constructionSite = undefined;
+                    creep.memory.task = undefined;
                 }
             }
         } else {
