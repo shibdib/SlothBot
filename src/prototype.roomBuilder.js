e89f9295417766b88eac2336692736729532fb16
shibdib <supernfadmin@gmail.com>
2018-04-14 05:40:43 -0400

    tweak

--- src/prototype.roomBuilder.js
+++ src/prototype.roomBuilder.js
@@ -350,6 +350,7 @@ function buildLabs(room, structures) {
         let sites = _.filter(room.constructionSites, (s) => s.structureType === STRUCTURE_LAB);
         if (lab.length + sites.length < 2 && terminal) {
             let safeZone = shuffle(room.lookForAtArea(LOOK_TERRAIN, terminal.pos.y - 2, terminal.pos.x - 2, terminal.pos.y + 2, terminal.pos.x + 2, true));
+            let hub = new RoomPosition(room.memory.extensionHub.x, room.memory.extensionHub.y, room.name);
             for (let key in safeZone) {
                 let position = new RoomPosition(safeZone[key].x, safeZone[key].y, room.name);
                 if (position.getRangeTo(terminal.pos) === 2) {
