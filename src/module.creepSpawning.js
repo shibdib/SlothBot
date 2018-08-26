7a3d7d3a966cb530c0594cf2ff51197a7f5758c9
shibdib <supernfadmin@gmail.com>
2018-08-26 02:56:04 -0400

    tweak

--- src/module.creepSpawning.js
+++ src/module.creepSpawning.js
@@ -285,7 +285,7 @@ module.exports.workerCreepQueue = function (room) {
         if (upgraders.length) priority = priority + upgraders.length;
         let number = 1;
         if (level < 4) number = _.round((20 - level) / 2);
-        if (upgraders.length < number) {
+        if (upgraders.length < number || (upgraders[0].ticksToLive < 100 && upgraders.length < number + 1)) {
             queueCreep(room, priority, {role: 'upgrader'})
         }
     }
