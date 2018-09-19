ae221e95e18b220567405847d53faf165ab6ebf7
shibdib <supernfadmin@gmail.com>
2018-09-18 23:03:13 -0400

    tweak

--- src/prototype.creep.js
+++ src/prototype.creep.js
@@ -359,6 +359,8 @@ Creep.prototype.reportDamage = function () {
     if (this.hits === this.hitsMax) {
         this.memory.healsPlease = undefined;
         this.memory.healsInbound = undefined;
+    } else {
+        this.memory.healsPlease = true;
     }
     if (this.memory.healsInbound) {
         if (!Game.getObjectById(this.memory.healsInbound)) this.memory.healsInbound = undefined;
@@ -367,7 +369,6 @@ Creep.prototype.reportDamage = function () {
         return this.memory._lastHits = this.hits;
     }
     if (this.hits < this.memory._lastHits) {
-        this.memory.healsPlease = true;
         this.memory.underAttack = true;
         if (this.room.controller && ((this.room.controller.owner && this.room.controller.owner.username !== USERNAME) || (this.room.controller.reservation && this.room.controller.reservation.username !== USERNAME)) && this.memory.targetRoom !== this.room.name) return false;
         let nearbyCreeps = _.uniq(_.pluck(_.filter(this.room.creeps, (c) => c.pos.getRangeTo(this) <= 3 && c.owner.username !== 'Invader' && c.owner.username !== 'Source Keeper' && c.owner.username !== USERNAME), 'owner.username'));
