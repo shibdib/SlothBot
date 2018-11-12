114fc24b0a9a06d7c3052007e2398d20757f958f
shibdib <supernfadmin@gmail.com>
2018-11-11 21:17:10 -0500

    git plz

--- src/role.filler.js
+++ src/role.filler.js
@@ -11,7 +11,7 @@ module.exports.role = function (creep) {
     if (!_.sum(creep.carry)) creep.memory.hauling = undefined;
     if (creep.memory.hauling) {
         // Hub Container
-        let hubContainer = Game.getObjectById(creep.room.memory.hubContainer) || creep.room.storage;
+        let hubContainer = Game.getObjectById(creep.room.memory.hubContainer) || creep.room.storage || _.filter(creep.room.structures, (s) => s.structureType === STRUCTURE_SPAWN)[0];
         if (hubContainer) {
             if (_.sum(hubContainer.store) === hubContainer.storeCapacity) return creep.idleFor(10);
             let storageItem = hubContainer;
@@ -30,6 +30,6 @@ module.exports.role = function (creep) {
                     if (storageItem.memory) delete storageItem.memory.deliveryIncoming;
                     break;
             }
-        } else creep.memory.recycle = true;
+        }
     } else if (creep.memory.energyDestination || creep.fillerEnergy()) creep.withdrawEnergy(); else creep.idleFor(5);
 };
