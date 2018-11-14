78da52cd5dbedefbb48204a16396b03479bbd1e9
shibdib <supernfadmin@gmail.com>
2018-11-13 21:33:24 -0500

    fix typo

--- src/role.healer.js
+++ src/role.healer.js
@@ -2,10 +2,7 @@
  * Created by Bob on 7/12/2017.
  */
 
-let _ = require('lodash');
-const profiler = require('screeps-profiler');
-
-function role(creep) {
+module.exports.role = function (creep) {
     if (!creep.memory.boostAttempt) return creep.tryToBoost(['heal']);
     if (creep.hits < creep.hitsMax) creep.heal(creep);
     // Harass
@@ -19,6 +16,4 @@ function role(creep) {
     let remoteGuardLeader = _.filter(Game.creeps, (c) => c.memory.overlord === creep.memory.overlord && c.memory.role === 'remoteGuard' && c.memory.squadLeader);
     if (!remoteGuardLeader.length) return creep.shibMove(new RoomPosition(25, 25, creep.memory.overlord), {range: 18});
     if (!creep.healMyCreeps() && !creep.healAllyCreeps() && creep.pos.getRangeTo(remoteGuardLeader[0]) > 2) return creep.shibMove(remoteGuardLeader[0]);
-}
-
-module.exports.role = profiler.registerFN(role, 'healerRole');
+};
