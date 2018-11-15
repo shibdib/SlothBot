23acdb43348b67dadd5a7bc55a01c50ca920c7a5
shibdib <supernfadmin@gmail.com>
2018-11-15 00:55:36 -0500

    fix a scout bug

--- src/operation.scout.js
+++ src/operation.scout.js
@@ -2,6 +2,11 @@ Creep.prototype.scoutRoom = function () {
     let sentence = [MY_USERNAME, 'Scout', 'Drone', 'For', this.memory.targetRoom];
     let word = Game.time % sentence.length;
     this.say(sentence[word], true);
+    // Handle edge case where room is overlord
+    if (this.memory.targetRoom === this.memory.overlord) {
+        delete Memory.targetRooms[this.room.name];
+        return this.memory.recycle = true;
+    }
     if (this.room.name !== this.memory.targetRoom) return this.shibMove(new RoomPosition(25, 25, this.memory.targetRoom), {
         range: 23,
         offRoad: true
