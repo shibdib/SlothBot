bd825eec8f315993b27f1255482a0d6dcd9b4707
shibdib <supernfadmin@gmail.com>
2019-01-28 05:18:45 -0500

    remove another layout

--- src/module.roomLayouts.js
+++ src/module.roomLayouts.js
@@ -1594,5 +1594,5 @@ let template12 = [
     },
 ];
 
-module.exports.layoutArray = [template1, template6, template7, template10, template12];
+module.exports.layoutArray = [template1, template6, template10, template12];
 
