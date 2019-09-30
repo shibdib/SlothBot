f1f705208f28d5a9253f61699559ca04ee5e4a5a
shibdib <supernfadmin@gmail.com>
2019-09-30 06:30:49 -0400

    raise threat if safemode is about to drop

--- src/military.defense.js
+++ src/military.defense.js
@@ -108,10 +108,10 @@ function safeModeManager(room) {
 
 function earlyWarning(room) {
     let adjacent = _.filter(Game.map.describeExits(room.name), (r) => Memory.roomCache[r] && Memory.roomCache[r].threatLevel >= 4)[0];
-    if (adjacent) {
+    if (adjacent || (room.controller.safeMode && room.controller.safeMode < 1700)) {
         Memory.roomCache[room.name].threatLevel = Memory.roomCache[adjacent].threatLevel;
         Memory.roomCache[room.name].tickDetected = Game.time;
-        log.a(roomLink(room.name) + ' has gone to threat level ' + Memory.roomCache[adjacent].threatLevel + ' due to a triggering of the early warning system in ' + roomLink(adjacent) + '.', 'DEFENSE COMMAND');
+        //log.a(roomLink(room.name) + ' has gone to threat level ' + Memory.roomCache[adjacent].threatLevel + ' due to a triggering of the early warning system in ' + roomLink(adjacent) + '.', 'DEFENSE COMMAND');
     }
 }
 
