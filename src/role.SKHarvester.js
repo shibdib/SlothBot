3e3ea818e806f03ac6989ee3a2fe8f632a58fdb5
shibdib <supernfadmin@gmail.com>
2019-10-10 14:25:19 -0400

    tweak sk harvester lair response

--- src/role.SKHarvester.js
+++ src/role.SKHarvester.js
@@ -11,6 +11,7 @@
 
 module.exports.role = function (creep) {
     if (creep.kite()) return true;
+    if (creep.hits < creep.hitsMax) return creep.goHomeAndHeal();
     if (creep.room.name === creep.memory.destination) {
         //If source is set mine
         if (!creep.memory.source && !creep.findSource()) creep.findMineral();
@@ -18,7 +19,7 @@ module.exports.role = function (creep) {
         // handle safe SK movement
         let lair = creep.pos.findInRange(creep.room.structures, 5, {filter: (s) => s.structureType === STRUCTURE_KEEPER_LAIR})[0];
         let SK = creep.pos.findInRange(creep.room.creeps, 5, {filter: (c) => c.owner.username === 'Source Keeper'})[0];
-        if (SK) return creep.kite(6); else if (lair && lair.ticksToSpawn <= 10) return creep.flee(lair);
+        if (SK) return creep.kite(6); else if (lair && lair.ticksToSpawn <= 10) return creep.flee(lair, 7);
         // Handle healing
         switch (creep.harvest(source)) {
             case ERR_NOT_IN_RANGE:
