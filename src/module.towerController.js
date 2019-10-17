22880d4ac4a18a6ebd8e5e8c70cdfeb725b8c84b
shibdib <supernfadmin@gmail.com>
2019-10-17 17:33:05 -0400

    only bother with nuke ramparts if there's a chance to make 5m

--- src/module.towerController.js
+++ src/module.towerController.js
@@ -28,11 +28,11 @@ module.exports.towerControl = function (room) {
             }
         } else if (room.memory.nuke) {
             let towers = _.shuffle(_.filter(structures, (s) => s.structureType === STRUCTURE_TOWER && s.isActive() && s.energy > s.energyCapacity * (s.pos.findClosestByRange(FIND_NUKES).timeToLand / NUKE_LAND_TIME)));
-            let nukeRampart = _.filter(room.structures, (s) => s.structureType === STRUCTURE_RAMPART && s.pos.getRangeTo(s.pos.findClosestByRange(FIND_NUKES)) <= 5 && s.hits < NUKE_DAMAGE[2])[0];
+            let nukeRampart = _.filter(room.structures, (s) => s.structureType === STRUCTURE_RAMPART && s.pos.getRangeTo(s.pos.findClosestByRange(FIND_NUKES)) <= 5 && s.hits < NUKE_DAMAGE[2] && (s.hits + ((towers.length * 500) * (s.pos.findClosestByRange(FIND_NUKES).timeToLand * 0.8))) >= NUKE_DAMAGE[2])[0];
             if (nukeRampart) {
                 for (let tower of towers) tower.repair(nukeRampart);
             } else {
-                nukeRampart = _.min(_.filter(room.structures, (s) => s.structureType === STRUCTURE_RAMPART && s.pos.getRangeTo(s.pos.findClosestByRange(FIND_NUKES)) <= 5), 'hits');
+                nukeRampart = _.min(_.filter(room.structures, (s) => s.structureType === STRUCTURE_RAMPART && s.pos.getRangeTo(s.pos.findClosestByRange(FIND_NUKES)) <= 5 && (s.hits + ((towers.length * 500) * (s.pos.findClosestByRange(FIND_NUKES).timeToLand * 0.8))) >= NUKE_DAMAGE[2]), 'hits');
                 for (let tower of towers) tower.repair(nukeRampart);
             }
         } else if (repairTower.energy > repairTower.energyCapacity * 0.5) {
