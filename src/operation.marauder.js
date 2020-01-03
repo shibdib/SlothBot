d91cff50317763f1d49035a16f4dd22f6a502bb9
shibdib <supernfadmin@gmail.com>
2020-01-03 03:36:38 -0500

    tweak marauder target finding

--- src/operation.marauder.js
+++ src/operation.marauder.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019.
+ * Copyright (c) 2020.
  * Github - Shibdib
  * Name - Bob Sardinia
  * Project - Overlord-Bot (Screeps)
@@ -10,12 +10,12 @@ Creep.prototype.marauding = function () {
     this.say(sentence[word], true);
     // Set a target
     if (!this.memory.targetRoom) {
-        let lowLevel = _.sample(_.filter(Memory.roomCache, (r) => r.name !== this.room.name && r.user && r.user !== MY_USERNAME && !_.includes(FRIENDLIES, r.user) && _.includes(Memory._threats, r.user) && !r.sk && !r.safemode && r.level && r.level < 3));
+        let lowLevel = _.sample(_.filter(Memory.roomCache, (r) => r.name !== this.room.name && r.user && r.user !== MY_USERNAME && !_.includes(FRIENDLIES, r.user) && !r.sk && !r.safemode && r.level && r.level < 3));
         if (lowLevel) {
             this.memory.targetRoom = lowLevel.name;
         } else {
-            let potential = _.sample(_.filter(Memory.roomCache, (r) => r.name !== this.room.name && r.user && r.user !== MY_USERNAME && !_.includes(FRIENDLIES, r.user) && _.includes(Memory._threats, r.user) && !r.sk && !r.level));
-            if (potential) this.memory.targetRoom = potential.name; else this.memory.operation = 'borderPatrol';
+            let potential = _.sample(_.filter(Memory.roomCache, (r) => r.name !== this.room.name && r.user && r.user !== MY_USERNAME && !_.includes(FRIENDLIES, r.user) && !r.sk && !r.level));
+            if (potential) this.memory.targetRoom = potential.name; else this.memory.recycle = true;
         }
     } else {
         if (this.room.name !== this.memory.targetRoom) {
