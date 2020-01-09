d268bc54337780428add0c9fe51748cfb7714c69
shibdib <supernfadmin@gmail.com>
2020-01-09 03:21:00 -0500

    fix initial drone hauling

--- src/role.drone.js
+++ src/role.drone.js
@@ -90,7 +90,7 @@ function hauling(creep) {
     if (!creep.room.controller || !creep.room.controller.owner || creep.room.controller.owner.username !== MY_USERNAME) return false;
     let haulers = _.filter(creep.room.creeps, (c) => c.my && c.memory && ((c.memory.role === 'drone' && c.memory.task === 'haul') || c.memory.role === 'hauler' || c.memory.role === 'filler' || (!creep.room.hubContainer && c.memory.role === 'filler')));
     let needyTower = _.filter(creep.room.structures, (s) => s.structureType === STRUCTURE_TOWER && s.energy < s.energyCapacity * 0.5).length > 0;
-    if (creep.memory.task === 'haul' || (creep.store[RESOURCE_ENERGY] === creep.store.getCapacity() && (!haulers.length || needyTower) && !creep.memory.task && (creep.room.energyAvailable < creep.room.energyCapacityAvailable || needyTower))) {
+    if (creep.memory.task === 'haul' || (creep.isFull && (!haulers.length || needyTower) && !creep.memory.task && (creep.room.energyAvailable < creep.room.energyCapacityAvailable || needyTower))) {
         creep.memory.task = 'haul';
         creep.say('Haul!', true);
         if (creep.memory.storageDestination || creep.haulerDelivery()) {
