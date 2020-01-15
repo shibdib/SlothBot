cb9712e3f4786e392118c88dce24237a6b2874a4
shibdib <supernfadmin@gmail.com>
2020-01-15 17:12:49 -0500

    increase likelihood controller link gets energy

--- src/module.linkController.js
+++ src/module.linkController.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020.
+ * Github - Shibdib
+ * Name - Bob Sardinia
+ * Project - Overlord-Bot (Screeps)
+ */
+
 /**
  * Created by Bob on 6/6/2017.
  */
@@ -17,7 +24,7 @@ module.exports.linkControl = function (room) {
         if (link.energy < 50) continue;
         let upgrader = _.filter(link.room.creeps, (c) => c.memory && c.memory.role === 'upgrader')[0];
         // Controller link if conditions met
-        if (upgrader && ((controllerLink && controllerLink.energy < 50 && Math.random() > 0.8) || (room.energyState && controllerLink && controllerLink.energy < 450))) {
+        if (upgrader && ((controllerLink && controllerLink.energy < 50 && Math.random() > 0.5) || (room.energyState && controllerLink && controllerLink.energy < 450))) {
             link.transferEnergy(controllerLink);
         } else if (storageLink && storageLink.energy < 500) {
             link.transferEnergy(storageLink);
