ed7c88b8916f406a871d1e36aebdde3c7ef4ce05
shibdib <supernfadmin@gmail.com>
2020-01-17 16:29:19 -0500

    tweak link ops

--- src/module.linkController.js
+++ src/module.linkController.js
@@ -6,10 +6,11 @@
  */
 
 module.exports.linkControl = function (room) {
-    let links = shuffle(_.filter(room.structures, (s) => s.structureType === STRUCTURE_LINK && s.cooldown === 0 && s.id !== s.room.memory.controllerLink && s.id !== s.room.memory.hubLink));
-    let storageLink = Game.getObjectById(room.memory.hubLink);
+    let links = _.sortBy(_.filter(room.structures, (s) => s.structureType === STRUCTURE_LINK && !s.cooldown && s.energy && s.id !== s.room.memory.controllerLink && s.id !== s.room.memory.hubLink), '.energy');
+    let hubLink = Game.getObjectById(room.memory.hubLink);
     let controllerLink = Game.getObjectById(room.memory.controllerLink);
     if (!controllerLink || !controllerLink.isActive()) delete room.memory.controllerLink;
+    if (!hubLink || !hubLink.isActive()) delete room.memory.hubLink;
     for (let link of links) {
         if (!controllerLink) {
             if (_.filter(link.pos.findInRange(room.structures, 2), (s) => s.structureType === STRUCTURE_CONTROLLER).length > 0) {
@@ -22,17 +23,19 @@ module.exports.linkControl = function (room) {
         // Controller link if conditions met
         if (upgrader && ((controllerLink && controllerLink.energy < 50 && Math.random() > 0.5) || (room.energyState && controllerLink && controllerLink.energy < 450))) {
             link.transferEnergy(controllerLink);
-        } else if (storageLink && storageLink.energy < 500) {
-            link.transferEnergy(storageLink);
+        } else if (hubLink && hubLink.energy < 500) {
+            link.transferEnergy(hubLink);
         } else if (controllerLink && controllerLink.energy < 700) {
             link.transferEnergy(controllerLink);
-        } else if (storageLink && storageLink.energy < 750) {
-            link.transferEnergy(storageLink);
+        } else if (hubLink && hubLink.energy < 750) {
+            link.transferEnergy(hubLink);
         } else if (_.filter(links, (l) => l.id !== link.id && l.energy < l.energyCapacity * 0.5 && l.energy < link.energy)[0]) {
             link.transferEnergy(_.filter(links, (l) => l.id !== link.id && l.energy < l.energyCapacity * 0.5)[0], link.energy * 0.5);
         }
     }
-    if (storageLink && controllerLink && storageLink.energy > 100 && controllerLink.energy < 250 && storageLink.room.energyAvailable > storageLink.room.energyCapacityAvailable * 0.95) {
-        storageLink.transferEnergy(controllerLink);
+    if (hubLink && controllerLink && hubLink.energy > 100 && controllerLink.energy < 250 && hubLink.room.energyAvailable > hubLink.room.energyCapacityAvailable * 0.95) {
+        hubLink.transferEnergy(controllerLink);
+    } else if (hubLink && controllerLink && controllerLink.energy > 400 && hubLink.energy < 50 && hubLink.room.energyAvailable < hubLink.room.energyCapacityAvailable * 0.95) {
+        controllerLink.transferEnergy(hubLink);
     }
 };
