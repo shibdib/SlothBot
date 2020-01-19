9ecf8047a62ab3e3c917d568edbce87e32fbdf62
shibdib <supernfadmin@gmail.com>
2020-01-19 06:09:03 -0500

    more spawn debugging

--- src/module.creepSpawning.js
+++ src/module.creepSpawning.js
@@ -21,8 +21,9 @@ module.exports.processBuildQueue = function () {
         let spawn = spawns[key];
         let level = getLevel(spawn.room);
         // Clear queue if something is stuck
-        if (lastBuilt[spawn.room.name] && roomQueue[spawn.room.name] && (Game.time - lastBuilt[spawn.room.name] >= 1450 || (level >= 3 && spawn.room.creeps.length < 4 && Math.random() > 0.5))) {
+        if (lastBuilt[spawn.room.name] && roomQueue[spawn.room.name] && (Game.time - lastBuilt[spawn.room.name] >= 1450)) {
             roomQueue[spawn.room.name] = undefined;
+            lastBuilt[spawn.room.name] = Game.time;
             continue;
         }
         if (!energyOrder[spawn.pos.roomName] || storedLevel[spawn.pos.roomName] !== level) determineEnergyOrder(spawn.room);
