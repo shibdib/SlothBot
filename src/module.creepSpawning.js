7066a118822193ffef6db34d2c89b50568f20bf7
shibdib <supernfadmin@gmail.com>
2020-01-22 05:58:41 -0500

    auxiliary targets always get queued regardless of distance

--- src/module.creepSpawning.js
+++ src/module.creepSpawning.js
@@ -35,7 +35,7 @@ module.exports.processBuildQueue = function () {
                 if (!spawn.room.memory.nuke && _.size(globalQueue) && !Memory.roomCache[spawn.room.name].responseNeeded && _.inRange(level, maxLevel - 1, maxLevel + 1)) {
                     let range = LOCAL_SPHERE;
                     if (spawn.room.energy > ENERGY_AMOUNT) range = LOCAL_SPHERE * 3;
-                    let distanceFilteredGlobal = _.filter(globalQueue, (q) => q.destination && ((Memory.auxiliaryTargets[q.destination] && Game.map.getRoomLinearDistance(q.destination, spawn.room.name) < 8) || Game.map.getRoomLinearDistance(q.destination, spawn.room.name) < range));
+                    let distanceFilteredGlobal = _.filter(globalQueue, (q) => q.destination && (Memory.auxiliaryTargets[q.destination] || Game.map.getRoomLinearDistance(q.destination, spawn.room.name) < range));
                     // If no energy surplus just urgent priority targets
                     if (spawn.room.energyState || spawn.room.energyAvailable === spawn.room.energyCapacityAvailable) {
                         queue = _.sortBy(Object.assign({}, distanceFilteredGlobal, roomQueue[spawn.room.name]), 'priority');
