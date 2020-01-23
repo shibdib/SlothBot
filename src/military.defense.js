aadf4cd95cfb72312799b05058b1e2cb2e946b06
shibdib <supernfadmin@gmail.com>
2020-01-23 02:14:09 -0500

    invaders will not cause room abandonment

--- src/military.defense.js
+++ src/military.defense.js
@@ -125,7 +125,7 @@ function unSavableCheck(room) {
     if (room.controller.safeMode || !room.hostileCreeps.length) return;
     let towers = _.filter(room.structures, (s) => s.structureType === STRUCTURE_TOWER && s.my);
     let badCount = room.memory.badCount || 0;
-    let hostiles = _.filter(room.hostileCreeps, (c) => c.getActiveBodyparts(ATTACK) || c.getActiveBodyparts(RANGED_ATTACK));
+    let hostiles = _.filter(room.hostileCreeps, (c) => c.owner.username !== 'Invader' && (c.getActiveBodyparts(ATTACK) || c.getActiveBodyparts(RANGED_ATTACK)));
     if (hostiles.length && !towers.length) {
         room.memory.badCount = badCount + 1;
         if (room.memory.badCount > room.controller.level + 2) {
