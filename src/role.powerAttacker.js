603fa76cd53db40203ced4f5186d72ff7d479ab5
shibdib <supernfadmin@gmail.com>
2020-02-01 18:24:36 -0500

    add missing return

--- src/role.powerAttacker.js
+++ src/role.powerAttacker.js
@@ -7,7 +7,7 @@
 
 module.exports.role = function (creep) {
     creep.memory.destinationReached = creep.pos.roomName === creep.memory.destination;
-    if (!Memory.auxiliaryTargets[creep.memory.destination]) creep.memory.recycle = true;
+    if (!Memory.auxiliaryTargets[creep.memory.destination]) return creep.memory.recycle = true;
     //Initial move
     if (!creep.memory.destinationReached) {
         creep.shibMove(new RoomPosition(25, 25, creep.memory.destination), {range: 23});
