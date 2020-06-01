d7c1b72acbf76d48b062cd39913035a7eda83f80
Bob Sardinia <shibdib@users.noreply.github.com>
2020-06-01 14:55:55 -0400

    fix overspawning pc

--- src/module.powerManager.js
+++ src/module.powerManager.js
@@ -20,10 +20,11 @@ module.exports.powerControl = function () {
     }
     // Handle PC spawning
     if (Game.gpl.level) {
-        let sparePowerLevels = Game.gpl.level - _.size(Game.powerCreeps);
-        let myRooms = _.filter(Memory.myRooms, (r) => Game.rooms[r].controller.level >= 8);
+        let sparePowerLevels = Game.gpl.level - (_.size(Game.powerCreeps) + _.sum(Game.powerCreeps, 'level'));
+        let myRooms = _.filter(Game.rooms, (r) => r.energyAvailable && r.controller.owner && r.controller.owner.username === MY_USERNAME && r.controller.level >= 8);
         if (_.size(Game.powerCreeps)) _.filter(Game.powerCreeps, (c) => c.level).forEach((c) => sparePowerLevels -= c.level);
-        if (sparePowerLevels && _.size(Game.powerCreeps) < myRooms.length) {
+        if (sparePowerLevels > 1 && _.size(Game.powerCreeps) < myRooms.length && (!global.LAST_POWER_CREEP_SPAWN || global.LAST_POWER_CREEP_SPAWN + 100 < Game.time)) {
+            global.LAST_POWER_CREEP_SPAWN = Game.time;
             let name = 'operator_' + _.random(1, 99);
             log.a('Created an operator named ' + name);
             PowerCreep.create(name, POWER_CLASS.OPERATOR);
@@ -31,9 +32,14 @@ module.exports.powerControl = function () {
             let powerCreeps = _.filter(Game.powerCreeps, (c) => c.my);
             for (let powerCreep of powerCreeps) {
                 if (powerCreep.ticksToLive) {
-                    powerCreep.memory.combat = undefined;
                     let powerCreepRole = require('powerRole.' + powerCreep.className);
                     try {
+                        // Handle suicide
+                        if (!powerCreep.level && sparePowerLevels <= 0) {
+                            console.log(powerCreep.name)
+                            powerCreep.suicide();
+                            continue;
+                        }
                         // If idle sleep
                         if (powerCreep.idle) continue;
                         // Handle nuke flee
@@ -44,11 +50,16 @@ module.exports.powerControl = function () {
                         log.e(e.stack);
                         Game.notify(e.stack);
                     }
-                } else if (!powerCreep.spawnCooldownTime || powerCreep.spawnCooldownTime < Date.now()) {
-                    let spawn = _.filter(Game.structures, (s) => s.my && s.structureType === STRUCTURE_POWER_SPAWN)[0];
-                    if (spawn) {
-                        log.a('Spawned an operator in ' + roomLink(spawn.room.name));
-                        powerCreep.spawn(spawn)
+                } else if (!powerCreep.deleteTime) {
+                    // Handle deleting
+                    if (!powerCreep.level && !sparePowerLevels) {
+                        powerCreep.delete();
+                    } else if (!powerCreep.spawnCooldownTime || powerCreep.spawnCooldownTime < Date.now()) {
+                        let spawn = _.filter(Game.structures, (s) => s.my && s.structureType === STRUCTURE_POWER_SPAWN)[0];
+                        if (spawn) {
+                            log.a('Spawned an operator in ' + roomLink(spawn.room.name));
+                            powerCreep.spawn(spawn)
+                        }
                     }
                 }
             }
