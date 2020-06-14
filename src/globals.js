28b902ab2ca3f7f0a56cf69040b924000c532b94
Bob Sardinia <shibdib@users.noreply.github.com>
2020-06-14 17:05:53 -0400

    cleanup

--- src/globals.js
+++ src/globals.js
@@ -432,21 +432,17 @@ let globals = function () {
 
     global.shuffle = function (array) {
         let counter = array.length;
-
         // While there are elements in the array
         while (counter > 0) {
             // Pick a random index
             let index = Math.floor(Math.random() * counter);
-
             // Decrease counter by 1
             counter--;
-
             // And swap the last element with it
             let temp = array[counter];
             array[counter] = array[index];
             array[index] = temp;
         }
-
         return array;
     };
 
