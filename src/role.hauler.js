37b087749d595d5b91add041c109d609923c7221
Bob Sardinia <shibdib@users.noreply.github.com>
2020-07-12 00:36:13 -0400

    code cleanup

--- src/role.hauler.js
+++ src/role.hauler.js
@@ -16,35 +16,12 @@ module.exports.role = function (creep) {
     if (creep.isFull) creep.memory.hauling = true;
     if (!_.sum(creep.store)) creep.memory.hauling = undefined;
     if (creep.memory.hauling) {
-        if (creep.memory.storageDestination || creep.haulerDelivery()) {
-            let storageItem = Game.getObjectById(creep.memory.storageDestination);
-            if (!storageItem) return delete creep.memory.storageDestination;
-            switch (creep.transfer(storageItem, RESOURCE_ENERGY)) {
-                case OK:
-                    delete creep.memory.storageDestination;
-                    delete creep.memory._shibMove;
-                    break;
-                case ERR_NOT_IN_RANGE:
-                    creep.shibMove(storageItem);
-                    break;
-                case ERR_FULL || ERR_INVALID_TARGET:
-                    delete creep.memory.storageDestination;
-                    delete creep.memory._shibMove;
-                    break;
-            }
-        } else creep.idleFor(5);
+        // Perform opportunistic road repair
+        creep.repairRoad();
+        creep.haulerDelivery();
+    } else if (creep.memory.energyDestination || creep.locateEnergy()) {
+        creep.withdrawResource()
     } else {
-        if (creep.memory.storageDestination || creep.haulerDelivery()) {
-            if (creep.memory.energyDestination || creep.locateEnergy(true)) {
-                if (creep.withdrawResource()) {
-                    if (creep.haulerDelivery()) {
-                        let storageItem = Game.getObjectById(creep.memory.storageDestination);
-                        creep.shibMove(storageItem);
-                    } else creep.idleFor(5);
-                }
-            }
-        } else {
-            creep.idleFor(10)
-        }
+        creep.idleFor(10)
     }
 };
