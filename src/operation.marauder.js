de8cc8d5bbdbb0fe4eb9d66fac8e275ee203a5fc
Bob Sardinia <shibdib@users.noreply.github.com>
2020-07-20 16:03:34 -0400

    tweak target finding

--- src/operation.marauder.js
+++ src/operation.marauder.js
@@ -20,15 +20,17 @@ Creep.prototype.marauding = function () {
     if (!this.memory.other.destination) {
         let roomName = this.room.name;
         let filtered = _.filter(Memory.roomCache, (r) => r.name !== roomName && !_.includes(this.memory.other.visited, r.name) && r.user && r.user !== MY_USERNAME && !_.includes(FRIENDLIES, r.user) && !r.sk && !r.safemode && !r.isHighway);
-        let target = _.sortBy(_.filter(filtered, (r) => _.includes(Memory._badBoyArray, r.user) && r.level && !r.towers), function (r) {
+        let target = _.sortBy(_.filter(filtered, (r) => _.includes(Memory._enemies, r.user) && r.level && !r.towers), function (r) {
             Game.map.getRoomLinearDistance(roomName, r.name);
-        })[0] || _.sortBy(_.filter(filtered, (r) => _.includes(Memory._badBoyArray, r.user) && !r.level), function (r) {
+        })[0] || _.sortBy(_.filter(filtered, (r) => _.includes(Memory._enemies, r.user) && !r.level), function (r) {
             Game.map.getRoomLinearDistance(roomName, r.name);
-        })[0] || _.sortBy(_.filter(filtered, (r) => Memory.ncpArray && _.includes(Memory.ncpArray, r.user) && !r.level), function (r) {
+        })[0] || _.sortBy(_.filter(filtered, (r) => _.includes(Memory._threatList, r.user) && r.level && !r.towers), function (r) {
             Game.map.getRoomLinearDistance(roomName, r.name);
-        })[0] || _.sortBy(_.filter(filtered, (r) => !_.includes(FRIENDLIES, r.user) && r.level && !r.towers && NEW_SPAWN_DENIAL), function (r) {
+        })[0] || _.sortBy(_.filter(filtered, (r) => _.includes(Memory._threatList, r.user) && !r.level), function (r) {
             Game.map.getRoomLinearDistance(roomName, r.name);
-        })[0] || _.sortBy(_.filter(filtered, (r) => !_.includes(FRIENDLIES, r.user) && !r.level), function (r) {
+        })[0] || _.sortBy(_.filter(filtered, (r) => r.level && !r.towers && NEW_SPAWN_DENIAL), function (r) {
+            Game.map.getRoomLinearDistance(roomName, r.name);
+        })[0] || _.sortBy(_.filter(filtered, (r) => !r.level && POKE_NEUTRALS), function (r) {
             Game.map.getRoomLinearDistance(roomName, r.name);
         })[0];
         if (target) {
@@ -38,15 +40,15 @@ Creep.prototype.marauding = function () {
             this.findDefensivePosition(this);
         }
     } else {
-        if (!this.canIWin() || (!this.handleMilitaryCreep() && !this.moveToHostileConstructionSites())) {
-            if (this.room.name !== this.memory.other.destination) {
-                return this.shibMove(new RoomPosition(25, 25, this.memory.other.destination), {range: 19});
-            }
-            if (this.room.name === this.memory.other.destination) {
+        if (this.room.name !== this.memory.other.destination) {
+            return this.shibMove(new RoomPosition(25, 25, this.memory.other.destination), {range: 19});
+        }
+        if (this.room.name === this.memory.other.destination) {
+            if (!this.canIWin() || (!this.handleMilitaryCreep() && !this.moveToHostileConstructionSites())) {
                 highCommand.generateThreat(this);
                 if (!this.memory.other.onScene) this.memory.other.onScene = Game.time;
                 // If on target and cant win find a new target
-                if (this.memory.other.onScene + 100 < Game.time) {
+                if (this.memory.other.onScene + 100 < Game.time || !this.canIWin()) {
                     this.room.cacheRoomIntel(true);
                     this.memory.other.visited.push(this.memory.other.destination);
                     this.memory.other.destination = undefined;
