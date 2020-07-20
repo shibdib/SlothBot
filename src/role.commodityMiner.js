44f0d72d5ed207876c14efabffa943642b8eb79f
Bob Sardinia <shibdib@users.noreply.github.com>
2020-07-20 16:04:29 -0400

    redo commodity mining

--- src/role.commodityMiner.js
+++ src/role.commodityMiner.js
@@ -10,38 +10,12 @@
  */
 
 module.exports.role = function (creep) {
+    if (!Memory.auxiliaryTargets[creep.memory.destination]) return creep.memory.recycle = true;
     //Invader detection
     if (creep.fleeHome()) return;
-    // Check if ready to haul
-    if (creep.isFull || (creep.memory.overlord === creep.pos.roomName && _.sum(creep.store)) || creep.ticksToLive <= 150) {
-        creep.memory.deposit = undefined;
-        if (creep.pos.roomName === creep.memory.overlord) {
-            if (creep.memory.storageDestination) {
-                let storageItem = Game.getObjectById(creep.memory.storageDestination);
-                for (const resourceType in creep.store) {
-                    switch (creep.transfer(storageItem, resourceType)) {
-                        case OK:
-                            creep.memory.storageDestination = undefined;
-                            if (creep.ticksToLive < 150) creep.memory.recycle = true;
-                            break;
-                        case ERR_NOT_IN_RANGE:
-                            creep.shibMove(storageItem);
-                            break;
-                        case ERR_FULL:
-                            creep.memory.storageDestination = undefined;
-                            break;
-                    }
-                }
-            } else {
-                let storage = creep.room.terminal || creep.room.storage;
-                if (storage) creep.memory.storageDestination = storage.id;
-            }
-        } else {
-            return creep.shibMove(new RoomPosition(25, 25, creep.memory.overlord), {range: 22});
-        }
-    } else if (creep.room.name !== creep.memory.destination) {
+    if (creep.room.name !== creep.memory.destination) {
         return creep.shibMove(new RoomPosition(25, 25, creep.memory.destination), {range: 22, offRoad: true});
-    } else if (creep.memory.deposit) {
+    } else if (creep.memory.deposit && !creep.isFull) {
         let deposit = Game.getObjectById(creep.memory.deposit);
         switch (creep.harvest(deposit)) {
             case OK:
