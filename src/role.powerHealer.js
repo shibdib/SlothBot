01af542b3a72c016a10ba28d89f2afb1f31c4acf
Bob Sardinia <shibdib@users.noreply.github.com>
2020-08-09 23:14:09 -0400

    power healer pairing

--- src/role.powerHealer.js
+++ src/role.powerHealer.js
@@ -13,6 +13,20 @@ module.exports.role = function (creep) {
     } else {
         let powerBank = _.filter(creep.room.structures, (s) => s.structureType === STRUCTURE_POWER_BANK)[0];
         if (powerBank && creep.pos.isNearTo(powerBank)) creep.moveRandom();
-        creep.healMyCreeps();
+        if (creep.memory.assigned) {
+            let assignment = Game.getObjectById(creep.memory.assigned);
+            if (!assignment) return creep.memory.assigned = undefined;
+            switch (creep.heal(assignment)) {
+                case ERR_NOT_IN_RANGE:
+                    creep.shibMove(assignment);
+                    creep.rangedHeal(assignment);
+            }
+        } else {
+            let attacker = _.filter(creep.room.creeps, (c) => c.my && c.memory.role === 'powerAttacker' && !_.filter(creep.room.creeps, (h) => h.my && h.memory.assigned === c.id)[0])[0];
+            if (attacker) creep.memory.assigned = attacker.id; else {
+                if (creep.pos.getRangeTo(powerBank) > 2) creep.shibMove(powerBank, {range: 2});
+                creep.healInRange();
+            }
+        }
     }
 };
