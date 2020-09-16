0586a7b5133d6e7717a22d256b109ec408d291b5
Bob Sardinia <shibdib@users.noreply.github.com>
2020-09-16 16:22:53 -0400

    tweak when builders needed

--- src/module.roomState.js
+++ src/module.roomState.js
@@ -34,5 +34,5 @@ module.exports.setRoomState = function (room) {
 };
 
 function requestBuilders(room) {
-    room.memory.buildersNeeded = !_.filter(room.structures, (s) => s.structureType === STRUCTURE_SPAWN).length || !_.filter(room.structures, (s) => s.structureType === STRUCTURE_TOWER).length || room.controller.level !== getLevel(room);
+    room.memory.buildersNeeded = !_.filter(room.structures, (s) => s.structureType === STRUCTURE_SPAWN).length || !_.filter(room.structures, (s) => s.structureType === STRUCTURE_TOWER).length;
 }
