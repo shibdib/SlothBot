00f42f1fa9e1816be4a99013a7a4df209aaf44ee
Shibdib <robert.sardinia@gmail.com>
2023-09-27 13:01:50 -0400

    Fix drones praising instead of building

--- src/role.drone.js
+++ src/role.drone.js
@@ -136,7 +136,7 @@ function hauling(creep) {
 function upgrading(creep, force = undefined) {
     if (creep.memory.task && creep.memory.task !== 'upgrade') return;
     if (!force) {
-        let controllerCheck = !creep.room.controller || !creep.room.controller.owner || creep.room.controller.owner.username !== MY_USERNAME || creep.room.controller.upgradeBlocked || creep.room.controller.level < 8 || !creep.room.controller.ticksToDowngrade || creep.room.controller.ticksToDowngrade > CREEP_LIFE_TIME * 2;
+        let controllerCheck = !creep.room.controller || !creep.room.controller.owner || creep.room.controller.owner.username !== MY_USERNAME || creep.room.controller.upgradeBlocked || creep.room.controller.level === 8 || !creep.room.controller.ticksToDowngrade || creep.room.controller.ticksToDowngrade > CREEP_LIFE_TIME * 2;
         if (!controllerCheck) {
             creep.memory.task = undefined;
             return false;
