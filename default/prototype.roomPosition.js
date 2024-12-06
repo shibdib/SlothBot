64e4bba6f10b08ce99d9c5b03256244929a141cb
Bob S <shibdib@users.noreply.github.com>
2024-12-06 16:27:16 -0500

    Fix occupied source check

--- default/prototype.roomPosition.js
+++ default/prototype.roomPosition.js
@@ -20,9 +20,9 @@ RoomPosition.prototype.checkIfOutOfBounds = function () {
  * @returns {*}
  */
 RoomPosition.prototype.getClosestSource = function () {
-    let source = this.findClosestByRange(FIND_SOURCES_ACTIVE, {filter: (s) => s.pos.countOpenTerrainAround() > _.filter(Game.rooms[this.roomName].creeps, (c) => c.memory && c.memory.source === s.id).length});
+    let source = this.findClosestByRange(FIND_SOURCES_ACTIVE, {filter: (s) => s.pos.countOpenTerrainAround() > _.filter(Game.rooms[this.roomName].creeps, (c) => c.memory && c.memory.other.source === s.id).length});
     if (!source) {
-        source = this.findClosestByRange(FIND_SOURCES, {filter: (s) => s.pos.countOpenTerrainAround() > _.filter(Game.rooms[this.roomName].creeps, (c) => c.memory && c.memory.source === s.id).length});
+        source = this.findClosestByRange(FIND_SOURCES, {filter: (s) => s.pos.countOpenTerrainAround() > _.filter(Game.rooms[this.roomName].creeps, (c) => c.memory && c.memory.other.source === s.id).length});
     }
     return source;
 };
