56e0eb2181b6ea4e5c56657332db42bc9048b986
Bob S <shibdib@users.noreply.github.com>
2025-01-06 03:37:43 -0500

    stop making these border patrol

--- default/role.attacker.js
+++ default/role.attacker.js
@@ -35,8 +35,7 @@ class RoleAttacker {
     unassignedTasks(creep) {
         if (creep.memory.destination && creep.memory.destination !== creep.room.name) return creep.shibMove(new RoomPosition(25, 25, creep.memory.destination), {range: 22});
         if (!creep.handleMilitaryCreep()) {
-            creep.room.cacheRoomIntel(true);
-            creep.memory.operation = 'borderPatrol';
+            creep.room.cacheRoomIntel();
             creep.memory.destination = undefined;
             creep.findDefensivePosition(creep);
         }
