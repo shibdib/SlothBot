932db4fe571e43b627388d11e00a363addf7a212
Bob S <shibdib@users.noreply.github.com>
2025-01-24 15:16:35 -0500

    force intel refresh if clearing destination

--- default/role.attacker.js
+++ default/role.attacker.js
@@ -35,8 +35,10 @@ class RoleAttacker {
     unassignedTasks(creep) {
         if (creep.memory.destination && creep.memory.destination !== creep.room.name) return creep.shibMove(new RoomPosition(25, 25, creep.memory.destination), {range: 22});
         if (!creep.handleMilitaryCreep()) {
-            creep.room.cacheRoomIntel();
-            creep.memory.destination = undefined;
+            if (creep.memory.destination) {
+                creep.room.cacheRoomIntel(true);
+                creep.memory.destination = undefined;
+            }
             creep.findDefensivePosition(creep);
         }
     }
