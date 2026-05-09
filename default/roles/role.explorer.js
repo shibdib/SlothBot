0929ebadc7d0bcd5d59a1cc38cb3177c6d1eaced
Bob S <shibdib@users.noreply.github.com>
2026-05-09 19:52:36 -0400

    explorer tweaks

--- default/roles/role.explorer.js
+++ default/roles/role.explorer.js
@@ -30,8 +30,8 @@ class RoleExplorer {
         let portal = Game.getObjectById(this.creep.memory.portal) || this.creep.pos.findClosestByRange(_.filter(this.creep.room.structures, (s) => s.structureType === STRUCTURE_PORTAL && !s.destination.shard));
         if (!this.creep.memory.usedPortal && portal && (this.creep.memory.other.portalJump || Math.random() > 0.01)) {
             if (!this.creep.memory.other.portalJump) {
-                let roomName;
-                if (portal.destination.shard) roomName = portal.destination.room.name; else roomName = portal.destination.roomName;
+                // inter-shard: destination.room is already a string; same-shard: destination.roomName
+                const roomName = portal.destination.shard ? portal.destination.room : portal.destination.roomName;
                 this.creep.memory.other.portalJump = roomName;
                 if (!this.creep.memory.portal) log.a(this.creep.name + ' has found a portal in ' + roomLink(this.room.name) + ' and is taking it.')
                 this.creep.memory.portal = portal.id;
@@ -41,14 +41,29 @@ class RoleExplorer {
             return this.creep.shibMove(portal, {range: 0});
         } else {
             const exits = Game.map.describeExits(this.room.name);
-            const rooms = Object.keys(exits).map((direction) => {
-                return {name: exits[direction], direction: parseInt(direction, 10)};
+            const rooms = Object.keys(exits).map(direction => ({
+                name: exits[direction],
+                direction: parseInt(direction, 10)
+            }));
+
+            let adjacent = _.filter(rooms, r => {
+                if (roomStatus(r.name) === 'closed') return false;
+                // Skip rooms we know are hostile or enemy-owned
+                const intel = INTEL[r.name];
+                if (intel && (intel.hostile || (intel.owner && !FRIENDLIES.includes(intel.owner)))) return false;
+                return pathableExit(this.creep, this.room.find(r.direction)[Math.floor(this.room.find(r.direction).length / 2)]);
             });
-            let adjacent = _.filter(rooms, (r) => !this.room.lookForAt(LOOK_STRUCTURES, this.room.find(r.direction)[0])[0] && roomStatus(r.name) !== 'closed' && pathableExit(this.creep, this.room.find(r.direction)[0]));
-            // Filter out the last room if we have options
-            if (this.creep.memory.lastRoom && adjacent.length > 1) adjacent = _.filter(adjacent, (a) => a.name !== this.creep.memory.lastRoom);
-            // If there's unexplored prioritize else pick random
-            let target = _.find(adjacent, (r) => !INTEL[r.name]) || _.sample(adjacent);
+
+            // Don't backtrack unless it's the only option
+            if (this.creep.memory.lastRoom && adjacent.length > 1) {
+                adjacent = _.filter(adjacent, a => a.name !== this.creep.memory.lastRoom);
+            }
+
+            // Priority: no intel > stale intel (oldest first) > random
+            let target = _.find(adjacent, r => !INTEL[r.name])
+                || _.min(adjacent.filter(r => INTEL[r.name]), r => INTEL[r.name].lastSeen || 0)
+                || _.sample(adjacent);
+
             if (target && target.name) this.creep.memory.destination = target.name; else this.creep.idleFor(6);
         }
     }
@@ -66,15 +81,21 @@ class RoleExplorer {
     signRooms() {
         if (this.room.controller && (!this.room.controller.sign || this.room.controller.sign.username !== MY_USERNAME) && this.creep.pos.findClosestByPath(this.room.controller)) {
             switch (this.creep.signController(this.room.controller, _.sample(EXPLORED_ROOM_SIGNS) + ` - ` + Game.time)) {
+                case OK:
+                    this.creep.memory.signAttempt = undefined;
+                    break;
                 case ERR_NOT_IN_RANGE:
                     if (!this.creep.memory.signAttempt) this.creep.memory.signAttempt = Game.time;
                     else if (this.creep.memory.signAttempt + 50 < Game.time) {
                         this.creep.memory.signAttempt = undefined;
-                        return this.creep.memory.lastRoom = this.room.name;
+                        this.creep.memory.lastRoom = this.room.name;
+                        return;
                     }
-                    return this.creep.shibMove(this.room.controller);
+                    this.creep.shibMove(this.room.controller);
+                    return;
             }
         }
+        this.creep.memory.signAttempt = undefined;
         this.creep.memory.lastRoom = this.room.name;
     }
 }
