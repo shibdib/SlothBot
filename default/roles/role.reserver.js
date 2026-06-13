3ff8cce9dafe82b9ab9e7da8612d1a009a8f7cdf
Bob S <shibdib@users.noreply.github.com>
2026-06-13 13:32:21 -0400

    Refactor reserver logic to enhance controller handling and improve movement efficiency

--- default/roles/role.reserver.js
+++ default/roles/role.reserver.js
@@ -16,64 +16,112 @@ class RoleReserver {
             this.creep.memory.other.stationary = undefined;
             return true;
         } else if (this.room.name !== this.creep.memory.destination) {
-            this.travel()
+            this.travel();
         } else if (!this.creep.memory.inPlace) {
             this.getToController();
-        } else if (!this.room.controller.reservation || this.room.controller.reservation.username === MY_USERNAME) {
-            this.reserveController();
-        } else if (this.room.controller.reservation && !FRIENDLIES.includes(this.room.controller.reservation.username)) {
-            this.attackController();
         } else {
-            this.creep.recycleCreep();
+            const controller = this.resolveController();
+            if (!controller) {
+                this.creep.memory.inPlace = undefined;
+                this.getToController();
+                return;
+            }
+            if (!controller.reservation || controller.reservation.username === MY_USERNAME) {
+                this.reserveController();
+            } else if (!FRIENDLIES.includes(controller.reservation.username)) {
+                this.attackController();
+            } else {
+                this.creep.recycleCreep();
+            }
         }
+    }
+
+    resolveController() {
+        if (this.room.controller) return this.room.controller;
+        const found = this.room.find(FIND_STRUCTURES, {filter: {structureType: STRUCTURE_CONTROLLER}});
+        return found[0];
+    }
+
+    getControllerPos() {
+        const controller = this.resolveController();
+        if (controller) return controller.pos;
+        if (this.creep.memory.controllerTarget) {
+            try {
+                const cached = JSON.parse(this.creep.memory.controllerTarget);
+                if (cached && cached.x !== undefined && cached.y !== undefined) {
+                    return new RoomPosition(cached.x, cached.y, this.room.name);
+                }
+            } catch (e) { /* fall through */
+            }
+        }
+        return new RoomPosition(25, 25, this.room.name);
     }
 
     travel() {
         let destination = new RoomPosition(25, 25, this.creep.memory.destination);
         if (this.creep.memory.controllerTarget) {
-            const controller = JSON.parse(this.creep.memory.controllerTarget)
-            destination = new RoomPosition(controller.x, controller.y, this.creep.memory.destination);
-            return this.creep.shibMove(destination);
+            try {
+                const controller = JSON.parse(this.creep.memory.controllerTarget);
+                if (controller && controller.x !== undefined && controller.y !== undefined) {
+                    destination = new RoomPosition(controller.x, controller.y, this.creep.memory.destination);
+                    return this.creep.shibMove(destination);
+                }
+            } catch (e) { /* use room center */
+            }
         }
         this.creep.shibMove(destination, {range: 23});
     }
 
     getToController() {
-        this.creep.memory.controllerTarget = JSON.stringify(this.room.controller.pos);
-        if (!this.creep.pos.isNearTo(this.room.controller)) return this.creep.shibMove(this.room.controller); else this.creep.memory.inPlace = true;
+        const controller = this.resolveController();
+        const pos = controller ? controller.pos : this.getControllerPos();
+        if (controller) this.creep.memory.controllerTarget = JSON.stringify(controller.pos);
+        if (!this.creep.pos.isNearTo(pos)) {
+            return this.creep.shibMove(controller || pos);
+        }
+        this.creep.memory.inPlace = true;
     }
 
     reserveController() {
-        switch (this.creep.reserveController(this.room.controller)) {
+        const controller = this.resolveController();
+        if (!controller) return;
+        switch (this.creep.reserveController(controller)) {
             case OK:
                 this.creep.memory.other.stationary = true;
                 if (!this.creep.memory.signed) {
                     let signs = RESERVE_ROOM_SIGNS;
-                    this.creep.signController(this.creep.room.controller, _.sample(signs));
+                    this.creep.signController(controller, _.sample(signs));
                     this.creep.memory.signed = true;
                 }
                 break;
             case ERR_NOT_IN_RANGE:
-                this.creep.shibMove(this.room.controller);
+                this.creep.shibMove(controller);
         }
-        const ticks = this.room.controller.reservation && this.room.controller.reservation.username === MY_USERNAME ? this.room.controller.reservation.ticksToEnd : 0;
-        INTEL[this.room.name].reservationExpires = Game.time + ticks;
-        if (!INTEL[this.room.name].reserverCap) INTEL[this.room.name].reserverCap = this.room.controller.pos.countOpenTerrainAround();
+        const intel = INTEL[this.room.name];
+        if (intel) {
+            const ticks = controller.reservation && controller.reservation.username === MY_USERNAME
+                ? controller.reservation.ticksToEnd : 0;
+            intel.reservationExpires = Game.time + ticks;
+            if (!intel.reserverCap) intel.reserverCap = controller.pos.countOpenTerrainAround();
+        }
     }
 
     attackController() {
-        switch (this.creep.attackController(this.room.controller)) {
+        const controller = this.resolveController();
+        if (!controller) return;
+        switch (this.creep.attackController(controller)) {
             case OK:
                 this.creep.memory.other.stationary = true;
                 if (!this.creep.memory.signed) {
                     let signs = RESERVE_ROOM_SIGNS;
-                    this.creep.signController(this.room.controller, _.sample(signs));
+                    this.creep.signController(controller, _.sample(signs));
                     this.creep.memory.signed = true;
                 }
-                if (!INTEL[this.room.name].reserverCap) INTEL[this.room.name].reserverCap = this.room.controller.pos.countOpenTerrainAround();
+                const intel = INTEL[this.room.name];
+                if (intel && !intel.reserverCap) intel.reserverCap = controller.pos.countOpenTerrainAround();
                 break;
             case ERR_NOT_IN_RANGE:
-                this.creep.shibMove(this.room.controller);
+                this.creep.shibMove(controller);
         }
     }
 }
