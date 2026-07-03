80c599720b8e0a37f00e3b2465ee676bb3386f36
Bob S <shibdib@users.noreply.github.com>
2026-07-03 09:57:54 -0400

    Fix indentation in drone role houseKeeping method

--- default/roles/role.drone.js
+++ default/roles/role.drone.js
@@ -26,7 +26,7 @@ class RoleDrone {
     }
 
     houseKeeping() {
-// SK Safety - Throttled
+        // SK Safety - Throttled
         if ((this.room.memory.sk || (INTEL[this.room.name] && INTEL[this.room.name].sk)) && this.creep.skSafety()) return true;
 
         // Boosting
